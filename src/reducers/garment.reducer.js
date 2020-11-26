4dc0cb3ff4b169efa8f25cc0ee11417d933c8957
Eugene Pavlovets <y.pavlovets@pixelplex.io>
2020-11-26 12:02:34 +0300

    [DGX-105] Сhanges to the text

--- src/reducers/garment.reducer.js
+++ src/reducers/garment.reducer.js
@@ -8,75 +8,75 @@ const DEFAULT_FIELDS = Map({
   garmentsReceiveByName: List([
     {
       name: 'Bitcoin For Pizza',
-      receive: 'PNG renders different views, FBX file, GIF, Child NFTs',
+      receive: '3 X PNG renders different views, FBX file, GIF',
     },
     {
       name: 'HODL King',
-      receive: '3 X PNG renders different views, FBX file, GIF, Child NFTs',
+      receive: '3 x PNG renders different views, FBX file, MP4 video.',
     },
     {
       name: 'Decentralised Dress',
-      receive: '3 X PNG renders different views, FBX file, GIF, Child NFTs',
+      receive: '3 X PNG renders different views, FBX file, GIF',
     },
     {
       name: 'DAI DAI DAI',
-      receive: '3 X PNG renders different views, FBX file, GIF, Child NFTs',
+      receive: '3 X PNG renders different views, FBX file, GIF',
     },
     {
       name: 'The Puurse',
-      receive: '3 X PNG renders different views, FBX file, GIF, Child NFTs',
+      receive: '3 X PNG renders different views, FBX file, GIF',
     },
     {
       name: 'DAO Out',
-      receive: '3 X PNG renders different views, FBX file, GIF, Child NFTs',
+      receive: '3 X PNG renders different views, FBX file, GIF',
     },
     {
       name: 'When Lambo?',
-      receive: '3 X PNG renders different views, FBX file, GIF, Child NFTs',
+      receive: '3 X PNG renders different views, FBX file, GIF',
     },
     {
       name: 'The Whale Hunter',
-      receive: '3 X PNG renders different views, FBX file, GIF, Child NFTs',
+      receive: '3 X PNG renders different views, FBX file, GIF',
     },
     {
       name: 'Crypto Bitch',
-      receive: '3 X PNG renders different views, FBX file, GIF, Child NFTs',
+      receive: '3 X PNG renders different views, FBX file, GIF',
     },
     {
       name: 'The Shitcoin Jacket',
-      receive: '3 X PNG renders different views, FBX file, GIF, Child NFTs',
+      receive: '3 X PNG renders different views, FBX file, GIF',
     },
     {
       name: 'DeFi Summer (Male)',
-      receive: '3 X PNG renders different views, FBX file, GIF, Child NFTs',
+      receive: '3 X PNG renders different views, FBX file, GIF',
     },
     {
       name: 'Design Title: DeFi Summer (Female)',
-      receive: '3 X PNG renders different views, FBX file, GIF, Child NFTs',
+      receive: '3 X PNG renders different views, FBX file, GIF',
     },
     {
       name: 'Crypto Winter',
-      receive: '3 X PNG renders different views, FBX file, GIF, Child NFTs',
+      receive: '3 x renders different views, FBX file, MP4 video.',
     },
     {
       name: 'Incognito',
-      receive: '3 X PNG renders different views, FBX file, GIF, Child NFTs',
+      receive: '3 x renders different views, FBX file, MP4 video.',
     },
     {
       name: 'To the Moon',
-      receive: '3 X PNG renders different views, FBX file, GIF, Child NFTs',
+      receive: '3 X PNG renders different views, FBX file, GIF',
     },
     {
       name: 'ICO Suit',
-      receive: '3 X PNG renders different views, FBX file, GIF, Child NFTs',
+      receive: '3 X PNG renders different views, FBX file, GIF',
     },
     {
       name: 'DeFi Shoe',
-      receive: '3 X PNG renders different views, FBX file, GIF, Child NFTs',
+      receive: '3 X PNG renders different views, FBX file, GIF',
     },
     {
       name: 'Lightning Network',
-      receive: '3 X PNG renders different views, FBX file, GIF, Child NFTs',
+      receive: '3 X PNG renders different views, FBX file, GIF',
     },
   ]),
 });
