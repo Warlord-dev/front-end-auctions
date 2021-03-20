74b32db7efce82125a276ee762452d8dcb7d7286
Tony Zhou <tony.zhou.vue@gmail.com>
2021-03-19 19:46:03 -0700

    installed biconomy

--- src/actions/bid.actions.js
+++ src/actions/bid.actions.js
@@ -1,16 +1,21 @@
 import BaseActions from '@actions/base-actions';
 import { utils as ethersUtils } from 'ethers';
 import { convertToWei } from '@helpers/price.helpers';
-import { getMarketplaceContractAddressByChainId, getMonaContractAddressByChainId } from '@services/network.service';
+import {
+  getMarketplaceContractAddressByChainId,
+  getMonaContractAddressByChainId,
+} from '@services/network.service';
 
 import auctionReducer from '../reducers/auction.reducer';
-import { getContract, getMarketplaceContract, getMonaTokenContract } from '../services/contract.service';
+import {
+  getContract,
+  getMarketplaceContract,
+  getMonaTokenContract,
+} from '../services/contract.service';
 
 class BidActions extends BaseActions {
-
   bid(id, value) {
     return async (_, getState) => {
-
       const account = getState().user.get('account');
       const auctionContractAddress = getState().global.get('auctionContractAddress');
       const contract = await getContract(auctionContractAddress);
@@ -27,24 +32,19 @@ class BidActions extends BaseActions {
           listener.off('transactionHash');
         },
       };
-
     };
-
   }
 
   getApprovedInMona() {
     return async (_, getState) => {
-
       const account = getState().user.get('account');
       const chainId = getState().global.get('chainId');
-      const marketplaceContract = await getMarketplaceContractAddressByChainId(
-        chainId,
-      );
-      const monaContractAddress = await getMonaContractAddressByChainId(
-        chainId,
-      );
+      const marketplaceContract = await getMarketplaceContractAddressByChainId(chainId);
+      const monaContractAddress = await getMonaContractAddressByChainId(chainId);
       const monaContract = await getMonaTokenContract(monaContractAddress);
-      const allowedValue = await monaContract.methods.allowance(account, marketplaceContract).call({ from: account });
+      const allowedValue = await monaContract.methods
+        .allowance(account, marketplaceContract)
+        .call({ from: account });
       const jsAllowedValue = parseFloat(ethersUtils.formatEther(allowedValue));
       return jsAllowedValue > 10000000000;
     };
@@ -52,22 +52,21 @@ class BidActions extends BaseActions {
 
   buyNow(id, value, isMona) {
     return async (_, getState) => {
-
       const account = getState().user.get('account');
       const chainId = getState().global.get('chainId');
-      const marketplaceContract = await getMarketplaceContractAddressByChainId(
-        chainId,
-      );
+      const marketplaceContract = await getMarketplaceContractAddressByChainId(chainId);
       const contract = await getMarketplaceContract(marketplaceContract);
       if (isMona) {
-        const monaContractAddress = await getMonaContractAddressByChainId(
-          chainId,
-        );
+        const monaContractAddress = await getMonaContractAddressByChainId(chainId);
         const monaContract = await getMonaTokenContract(monaContractAddress);
-        const allowedValue = await monaContract.methods.allowance(account, marketplaceContract).call({ from: account });
+        const allowedValue = await monaContract.methods
+          .allowance(account, marketplaceContract)
+          .call({ from: account });
         const jsAllowedValue = parseFloat(ethersUtils.formatEther(allowedValue));
         if (jsAllowedValue < 10000000000) {
-          const listener = monaContract.methods.approve(marketplaceContract, convertToWei(20000000000)).send({ from: account });
+          const listener = monaContract.methods
+            .approve(marketplaceContract, convertToWei(20000000000))
+            .send({ from: account });
           const promise = new Promise((resolve, reject) => {
             listener.on('error', (error) => reject(error));
             listener.on('transactionHash', (transactionHash) => resolve(transactionHash));
@@ -82,7 +81,9 @@ class BidActions extends BaseActions {
         }
       }
 
-      const listener = contract.methods.buyOffer(id, isMona).send({ from: account, value: isMona ? 0 : value });
+      const listener = contract.methods
+        .buyOffer(id, isMona)
+        .send({ from: account, value: isMona ? 0 : value });
       const promise = new Promise((resolve, reject) => {
         listener.on('error', (error) => reject(error));
         listener.on('transactionHash', (transactionHash) => resolve(transactionHash));
@@ -94,14 +95,11 @@ class BidActions extends BaseActions {
           listener.off('transactionHash');
         },
       };
-
     };
-
   }
 
   withdraw(id) {
     return async (_, getState) => {
-
       const account = getState().user.get('account');
       const auctionContractAddress = getState().global.get('auctionContractAddress');
       const contract = await getContract(auctionContractAddress);
@@ -119,9 +117,7 @@ class BidActions extends BaseActions {
         },
       };
     };
-
   }
-
 }
 
 export default new BidActions(auctionReducer);
