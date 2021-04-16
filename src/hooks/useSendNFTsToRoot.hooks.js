106df53632c9bf9758629c828012ec7a3371e8fb
FrozenIce0617 <prosdev087@gmail.com>
2021-04-16 21:43:38 +0100

    chore: update useSendNFTsToRoot hook

--- src/hooks/useSendNFTsToRoot.hooks.js
+++ src/hooks/useSendNFTsToRoot.hooks.js
@@ -2,22 +2,37 @@ import { useCallback } from 'react';
 import { useSelector } from 'react-redux';
 
 import { getAccount } from '@selectors/user.selectors';
-import { getDTXContract } from '@services/contract.service';
+import { getDTXContract, getDigiMaterialV2Contract } from '@services/contract.service';
+import config from '@utils/config';
+import { getApprovalStatus, setApprovalStatus } from '@utils/webStorage.util';
+import { useIsMainnet } from './useIsMainnet';
 
 const useSendNFTsToRoot = () => {
+  const isMainnet = useIsMainnet();
   const account = useSelector(getAccount);
+  const isApproved = getApprovalStatus();
+  const dtxAddress = config.DTX_ADDRESSES[isMainnet ? 'matic' : 'mumbai'];
 
   const sendNTFsToRoot = useCallback(
     async (tokenIds) => {
       try {
-        const dtxContract = await getDTXContract();
+        const dtxContract = getDTXContract();
+        const digiMaterialV2Contract = getDigiMaterialV2Contract();
+
+        if (!isApproved) {
+          await digiMaterialV2Contract.methods
+            .setApprovalForAll(dtxAddress, true)
+            .send({ from: account }, (error, hash) => {
+              console.log('CALLBACK ERR - ', error, hash);
+            });
+        }
         const res = await dtxContract.methods.sendNFTsToRoot(tokenIds).call({ from: account });
         return { success: true, result: res };
       } catch (e) {
         return { success: false, result: e };
       }
     },
-    [getDTXContract, account]
+    [getDTXContract, account],
   );
 
   return sendNTFsToRoot;
