1df4ccd93440c443b5452b072249073726f633ce
beowulf518 <beowulf93518@gmail.com>
2021-04-22 04:00:19 -0400

    added header

--- src/components/layouts/header-top-line/index.js
+++ src/components/layouts/header-top-line/index.js
@@ -1,7 +1,8 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Router, { useRouter }  from 'next/router';
 import PropTypes from 'prop-types';
+import throttle from 'lodash.throttle';
 import cn from 'classnames';
 import Link from 'next/link';
 import Button from '@components/buttons/button';
@@ -14,6 +15,27 @@ import LandingHeader from './landing';
 import styles from './styles.module.scss';
 
 const HeaderTopLine = ({ className, isShowStaking, buttonText, linkText }) => {
+  
+  const [hasScrolled, setHasScrolled] = useState(false)
+  const [isCollapse, setIsCollapse] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = throttle(() => {
+      const offset = 0
+      const { scrollTop } = document.documentElement
+      const scrolled = scrollTop > offset
+
+      if (hasScrolled !== scrolled) {
+        setHasScrolled(scrolled)
+      }
+    }, 200)
+
+    document.addEventListener('scroll', handleScroll)
+    return () => {
+      document.removeEventListener('scroll', handleScroll)
+    }
+  }, [hasScrolled])
+
   const dispatch = useDispatch();
   const user = useSelector(getUser);
   if (!user) {
@@ -21,6 +43,9 @@ const HeaderTopLine = ({ className, isShowStaking, buttonText, linkText }) => {
   }
 
   const handleClick = () => dispatch(openConnectMetamaskModal());
+  const onIconHander = () => {
+    setIsCollapse(!isCollapse);
+  };
 
   const [isShowMenu, setIsShowMenu] = useState(false);
 
@@ -42,7 +67,7 @@ const HeaderTopLine = ({ className, isShowStaking, buttonText, linkText }) => {
     <LandingHeader textColor={'#74A3F3'}/>
   )
   : (
-    <div className={cn(className, styles.wrapper)}>
+    <div className={cn(className, styles.wrapper, hasScrolled?styles.floatingNav:'')}>
       <div className={styles.leftBox}>
         <Logo />
         {/* <a href="https://skins.digitalax.xyz/" className={styles.goToMaticButton}>
@@ -54,22 +79,22 @@ const HeaderTopLine = ({ className, isShowStaking, buttonText, linkText }) => {
         </div> */}
       </div>
       <div className={styles.rightBox}>
-        <div className={styles.links}>
+        <div className={cn(styles.links, isCollapse?styles.expandedMenu:'')}>
           {/* <Link href="/">
             <a className={styles.link}>Auctions</a>
           </Link>
           <Link href="/sold">
             <a className={styles.link}>Previously Sold</a>
-          </Link> */}
-          {/* <a
+          </Link> */
+          /* <a
             href="https://pode.digitalax.xyz/"
             className={styles.link}
             target="_blank"
             rel="noreferrer"
           >
             PODE
-          </a> */}
-          <a
+          </a> */          
+          /* <a
             href="https://medium.com/@digitalax"
             className={styles.link}
             target="_blank"
@@ -94,40 +119,56 @@ const HeaderTopLine = ({ className, isShowStaking, buttonText, linkText }) => {
             >
               {linkText}
             </a>
-          )}
+          )} */}
+          <Link href="https://skins.digitalax.xyz">
+            <a className={styles.link} target="_blank">
+              SUIT UP IN YOUR GAME SKINS
+            </a>
+          </Link>
+          <Link href="https://marketplace.digitalax.xyz">
+            <a className={styles.link} target="_blank">
+              REP YOUR STLE IRL 
+            </a>
+          </Link>
           <Link href="/global">
             <a className={styles.link}>Global Designer Network</a>
           </Link>
-        </div>
-        {user ? (
-          <div className={styles.buttonWrapper}>
-            <SmallPhotoWithText
-              photo={user.get('avatar') ? user.get('avatar') : './images/user-photo.svg'}
-              address={user.get('username')}
-              className={styles.hashAddress}
-            >
-              <button className={styles.arrowBottom} onClick={() => setIsShowMenu(!isShowMenu)}>
-                <img
-                  className={styles.arrowBottomImg}
-                  src="./images/icons/arrow-bottom.svg"
-                  alt="arrow-bottom"
-                />
-              </button>
-            </SmallPhotoWithText>
-            {isShowMenu && (
-              <div className={styles.menuWrapper}>
-                <button onClick={() => handleProfileClick()} className={styles.menuButton}>
-                  Profile
-                </button>
-                <button onClick={() => handleLogoutClick()} className={styles.menuButton}>
-                  Logout
-                </button>
+          <Link href="http://staking.digitalax.xyz/">
+            <a className={styles.link}>STAKE YOUR FASHION</a>
+          </Link>
+          <div className={styles.signBtn}>
+            {user ? (
+              <div className={styles.buttonWrapper}>
+                <SmallPhotoWithText
+                  photo={user.get('avatar') ? user.get('avatar') : './images/user-photo.svg'}
+                  address={user.get('username')}
+                  className={styles.hashAddress}
+                >
+                  <button className={styles.arrowBottom} onClick={() => setIsShowMenu(!isShowMenu)}>
+                    <img
+                      className={styles.arrowBottomImg}
+                      src="./images/icons/arrow-bottom.svg"
+                      alt="arrow-bottom"
+                    />
+                  </button>
+                </SmallPhotoWithText>
+                {isShowMenu && (
+                  <div className={styles.menuWrapper}>
+                    <button onClick={() => handleProfileClick()} className={styles.menuButton}>
+                      Profile
+                    </button>
+                    <button onClick={() => handleLogoutClick()} className={styles.menuButton}>
+                      Logout
+                    </button>
+                  </div>
+                )}
               </div>
+            ) : (
+              <Button onClick={() => handleClick()}>{buttonText}</Button>
             )}
           </div>
-        ) : (
-          <Button onClick={() => handleClick()}>{buttonText}</Button>
-        )}
+          <a href="javascript:void(0);" className={styles.collapseIcon} onClick={onIconHander}>&#9776;</a>
+        </div>
       </div>
     </div>
   );
