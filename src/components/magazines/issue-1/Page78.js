4664002fcd5bbbfc92aad8a2347da4ec1023abac
wj-code-mania <wonyy1988@outlook.com>
2021-05-11 13:43:44 -0700

    feat: add text layout on Page 8

--- src/components/magazines/issue-1/Page78.js
+++ src/components/magazines/issue-1/Page78.js
@@ -52,19 +52,19 @@ const Page78 = () => {
       <div className={styles.logo_t}>T</div>
       <p className={styles.section1}>
         It's only elementary that people would be shocked to find out when acclaimed auction house Christie's sold a piece of digital NFT artwork for $69 million — for a piece of art that doesn't physically exist. 
-
+        <br /><br />
         The shock was mainly because most people find it hard to reconcile the value of a digital piece of art, whose production costs are next to nothing compared to the price it went for, and whose value is more closely tied to its scarcity (perceived or otherwise) than its actual utility.
-
+        <br /><br />
         The same is happening for the real estate boom that's happening right under the collective industry's nose, and it's happening in the corners of the metaverse—the virtual worlds where not only digital art, real estate, avatars, and fashion can be bought and sold as NFTs. The world where digital natives and their avatars can explore, socialize, play, and live.
       </p>
       <p className={styles.section2}>
-        he Value of Virtual Real Estate. Everybody gets digital art, but virtual real estate is a different thing. Why should anyone spend money on a virtual piece of real estate that doesn't exist in real life? What makes pixelated parcels of land on a virtual world accessible through a website a real, tangible asset?
-
+        <span>he Value of Virtual Real Estate.</span> Everybody gets digital art, but virtual real estate is a different thing. Why should anyone spend money on a virtual piece of real estate that doesn't exist in real life? What makes pixelated parcels of land on a virtual world accessible through a website a real, tangible asset?
+        <br /><br />
         This requires a bit more context. That said, if you've ever played a video game — doesn't matter if it's on the Atari or the NES, or if it's Super Mario, FIFA, Final Fantasy, Fortnite, or Call of Duty — then you would know that as your character explores the virtual world, you'd be spending oodles and oodles of time roaming through the vast expanses of the countless artificial universes these games offer.
       </p>
       <p className={styles.section3}>
         And the virtually limitless expanses of virtual worlds provide opportunities for brands to market, advertise, or engage with the emerging digital population. In-game advertising, after all, is a multi-billion-dollar industry that needs no further introduction.
-
+        <br /><br />
         Furthermore, virtual real estate presents an opportunity for companies to purchase virtual parcels of land within the metaverses that they form part of — and the possibilities are endless. Companies can build entire cityscapes within those metaverses and monetize them as they see fit— such as putting up a building and leasing out ad and store space, create living spaces that they can rent out, or any revenue-generating venture, for that matter. The possibilities are endless.
       </p>
       <p className={styles.section4}>
