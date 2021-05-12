e8a02b64ccf60c6b505604eeba818f70b3492144
jupiteruwdev <harutoinoue8@gmail.com>
2021-05-12 19:23:52 -0400

    hot fix for page 3, 38.

--- src/components/magazines/issue-1/Page3738.js
+++ src/components/magazines/issue-1/Page3738.js
@@ -1,72 +1,105 @@
-import React from 'react'
-import styles from './page3738.module.scss'
+import React from 'react';
+import styles from './page3738.module.scss';
 
 const Page3738 = () => {
   return (
     <div className={styles.wrapper}>
-      <img src='./magazine/1/images/Group 1179.jpg' className={styles.backImage} />
-      <h1>
-        The Long Thread of Textile History Continues
-      </h1>
-      <h2>
-        India Embraces Digital Fashion
-      </h2>
+      <img src="./magazine/1/images/Group 1179.jpg" className={styles.backImage} />
+      <h1>The Long Thread of Textile History Continues</h1>
+      <h2>India Embraces Digital Fashion</h2>
       <p className={styles.section1}>
-        We owe a lot of what constitutes the fashion industry, as we know of it today, to India. In fact, our earliest knowledge about the origins and use of cotton can be dated back to a series of famous Indian poems written in 600BC. And, even Alexander the Great is recorded wearing cotton clothes after invading India with his troops. The nation has contributed a whole threadline of rich history to fashion globally, with about 1/3 of India’s textile production exported orientated, and the US standing as the biggest importer of Indian textile.
-        <br /><br />
-        Thus, it only seems fated that the region is now embracing the new world of fashiontech, which has been propelled over the past few months with the rise in relevance of NFTs and their harmonous affinity for finally bridging value and authenticity to digital fashion. 
+        We owe a lot of what constitutes the fashion industry, as we know of it today, to India. In
+        fact, our earliest knowledge about the origins and use of cotton can be dated back to a
+        series of famous Indian poems written in 600BC. And, even Alexander the Great is recorded
+        wearing cotton clothes after invading India with his troops. The nation has contributed a
+        whole threadline of rich history to fashion globally, with about 1/3 of India’s textile
+        production exported orientated, and the US standing as the biggest importer of Indian
+        textile.
+        <br />
+        <br />
+        Thus, it only seems fated that the region is now embracing the new world of fashiontech,
+        which has been propelled over the past few months with the rise in relevance of NFTs and
+        their harmonous affinity for finally bridging value and authenticity to digital fashion.
       </p>
       <p className={styles.section2}>
-        We sat down with Ravi Singh, a newcomer to digital fashion yet avid supporter of inspiring traditional cultural inclusivity in the industry, and 3D desginer, Lorena Bello, to discuss their collaboration alongside DIGITALAX, on the world’s first Indian digital fashion collection. 
+        We sat down with Ravi Singh, a newcomer to digital fashion yet avid supporter of inspiring
+        traditional cultural inclusivity in the industry, and 3D desginer, Lorena Bello, to discuss
+        their collaboration alongside DIGITALAX, on the world’s first Indian digital fashion
+        collection.
       </p>
       <p className={styles.section3}>
-        Last year, if you had told me that I’d be writing for the world’s first metaverse magazine about my Indian digital fashion collection, designed in collaboration with Lorena Bello, a world-renowned 3d fashion designer, I would have said you were confusing me for a character in a Marvel movie. After being on Indian Matchmaking on Netflix in 2020, I started to connect with South Asian influencers and artists around the world. Through an Indian streetwear brand, I attended New York Fashion Week, which inspired me to explore how to fuse South Asian culture with American identity. This spring, I learned about DIGITALAX, the world’s first digital fashion operating system, and how it’s designed to liberate gaming and fashion mods from gatekeepers to the masses. 
+        Last year, if you had told me that I’d be writing for the world’s first metaverse magazine
+        about my Indian digital fashion collection, designed in collaboration with Lorena Bello, a
+        world-renowned 3d fashion designer, I would have said you were confusing me for a character
+        in a Marvel movie. After being on Indian Matchmaking on Netflix in 2020, I started to
+        connect with South Asian influencers and artists around the world. Through an Indian
+        streetwear brand, I attended New York Fashion Week, which inspired me to explore how to fuse
+        South Asian culture with American identity. This spring, I learned about DIGITALAX, the
+        world’s first digital fashion operating system, and how it’s designed to liberate gaming and
+        fashion mods from gatekeepers to the masses.
       </p>
       <p className={styles.section4}>
-        I was excited by the intersection of crypto and the metaverse – digital worlds layered on top of our physical reality. All these connections led me to want to build an Indian digital fashion collection, which is something I had never seen before. But this is more than about digital fashion – it’s about creating culturally inclusive spaces in the metaverse. It’s about inspiring more metaverse artists from all backgrounds, from Africa, to South America, to India, Pakistan and Bangladesh.
+        I was excited by the intersection of crypto and the metaverse – digital worlds layered on
+        top of our physical reality. All these connections led me to want to build an Indian digital
+        fashion collection, which is something I had never seen before. But this is more than about
+        digital fashion – it’s about creating culturally inclusive spaces in the metaverse. It’s
+        about inspiring more metaverse artists from all backgrounds, from Africa, to South America,
+        to India, Pakistan and Bangladesh.
       </p>
-      <div className={styles.name1}>
-        -Ravi Singh
-      </div>
+      <div className={styles.name1}>-Ravi Singh</div>
       <p className={styles.section5}>
-        The collection was inspired by Ravi, 3D modelled by Lorena and all scenes, animation and renders composed by DIGITALAX.
+        The collection was inspired by Ravi, 3D modelled by Lorena and all scenes, animation and
+        renders composed by DIGITALAX.
       </p>
       <p className={styles.section6}>
-        From my first conversation with Ravi, I was completely behind the idea of bridging traditional culture into digital fashion. The opportunity to be a part of the world’s first digital fashion line based on Indian culture + make a disruptive point in South Asian fashion culture and open a new frontier in the digital fashion space.... to help bear Indian fashion into the Metaverse... I absolutely wanted to be involved! 
-        <br /><br />
-        Not having a strong background in Indian Culture myself allowed me to work without preconceptions. After many virtual coffees NYC-Madrid I worked experimentally to best present the first cultural crossover into digital fashion.
+        From my first conversation with Ravi, I was completely behind the idea of bridging
+        traditional culture into digital fashion. The opportunity to be a part of the world’s first
+        digital fashion line based on Indian culture + make a disruptive point in South Asian
+        fashion culture and open a new frontier in the digital fashion space.... to help bear Indian
+        fashion into the Metaverse... I absolutely wanted to be involved!
+        <br />
+        <br />
+        Not having a strong background in Indian Culture myself allowed me to work without
+        preconceptions. After many virtual coffees NYC-Madrid I worked experimentally to best
+        present the first cultural crossover into digital fashion.
       </p>
-      <div className={styles.name2}>
-        -Lorena Bello
-      </div>
+      <div className={styles.name2}>-Lorena Bello</div>
       <video autoPlay muted loop className={styles.indiaNecklaceVideo}>
-        <source src='./magazine/1/videos/indiaNecklace.mp4' type="video/mp4" />
+        <source src="./magazine/1/videos/indiaNecklace.mp4" type="video/mp4" />
       </video>
-      <img src='./magazine/1/images/Group 1185.png' className={styles.circleImg1} />
+      <img src="./magazine/1/images/Group 1185.png" className={styles.circleImg1} />
       <div className={styles.circleText1}>
         The owl is a vehicle of Lakshmi, the goddess of wealth and good fortune in Hinduism.
       </div>
-      <img src='./magazine/1/images/Group 1186.png' className={styles.circleImg2} />
+      <img src="./magazine/1/images/Group 1186.png" className={styles.circleImg2} />
       <div className={styles.circleText2}>
-        The sherwani; a long coat-like jacket that is knee length and originated in 19th century India.<br /><br />
-        The jacket contains lotus flowers, which symbolize purity, enlightenment and self-regeneration— a popular symbol in India and around the world.
+        The sherwani; a long coat-like jacket that is knee length and originated in 19th century
+        India.
+        <br />
+        <br />
+        The jacket contains lotus flowers, which symbolize purity, enlightenment and
+        self-regeneration— a popular symbol in India and around the world.
       </div>
       <video autoPlay muted loop className={styles.indiaSuitVideo}>
-        <source src='./magazine/1/videos/indiaSuit.mp4' type="video/mp4" />
+        <source src="./magazine/1/videos/indiaSuit.mp4" type="video/mp4" />
       </video>
       <video autoPlay muted loop className={styles.indiaSuit2Video}>
-        <source src='./magazine/1/videos/IndiaDress.mp4' type="video/mp4" />
+        <source src="./magazine/1/videos/IndiaDress.mp4" type="video/mp4" />
       </video>
-      <img src='./magazine/1/images/Group 1187.png' className={styles.circleImg3} />
+      <img src="./magazine/1/images/Group 1187.png" className={styles.circleImg3} />
       <div className={styles.circleText3}>
-        A popular form of Indian women’s dress, the Lehenga Choli, 
-        which originated in North India during the 10th century and is often worn for weddings or other formal events. <br /><br />
-        The pattern on the skirt is a binary flower, fusing the traditional – a flower – with the futuristic – 0’s and 1’s used in binary codes popularized in the movie the Matrix. 
-        The blouse contains a cubed pattern containing my initials, RGS, and was sketched by hand by Nureen Wohl, a social worker based in New York City. 
-        The headscarf, or the dupatta, contains an intricate piece of forehead jewelry, called a maang tikka.
+        A popular form of Indian women’s dress, the Lehenga Choli, which originated in North India
+        during the 10th century and is often worn for weddings or other formal events.
+        <br />
+        <br />
+        The pattern on the skirt is a binary flower, fusing the traditional – a flower – with the
+        futuristic – 0’s and 1’s used in binary codes popularized in the movie the Matrix. The
+        blouse contains a cubed pattern containing Ravi’s initials, RGS, and was sketched by hand by
+        Nureen Wohl, a social worker based in New York City. The headscarf, or the dupatta, contains
+        an intricate piece of forehead jewelry, called a maang tikka.
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page3738
+export default Page3738;
