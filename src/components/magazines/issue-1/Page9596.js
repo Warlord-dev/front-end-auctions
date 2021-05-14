a718d315534dc7552ca03ab81dcc3a9fbf13f2b6
CameronS29 <c.sato.dev@gmail.com>
2021-05-14 19:49:42 -0400

    page 96 fixed.

--- src/components/magazines/issue-1/Page9596.js
+++ src/components/magazines/issue-1/Page9596.js
@@ -9,48 +9,89 @@ const Page8788 = () => {
       <div className={styles.textBack1}></div>
       <div className={styles.textBack2}></div>
       <div className={styles.textBack3}></div>
-      <div className={styles.textBack4}></div>
       <p className={styles.text1}>
-        <span>Raising Your Marketing Game</span><br />
-        We've all heard the "starving artist" trope. We've also heard the stereotypical assertion that artists aren't generally renowned marketers. But in this frenzied environment that art finds itself in, artists can only survive by marketing their works of art. Therefore, today's digital creators need an adequate marketing strategy to give themselves the best chance to succeed.
+        <span>Raising Your Marketing Game</span>
+        <br />
+        We've all heard the "starving artist" trope. We've also heard the stereotypical assertion
+        that artists aren't generally renowned marketers. But in this frenzied environment that art
+        finds itself in, artists can only survive by marketing their works of art. Therefore,
+        today's digital creators need an adequate marketing strategy to give themselves the best
+        chance to succeed.
       </p>
       <p className={styles.text2}>
-        Marketing is no longer about drawing attention in a saturated market; rather, it is about fostering dialog with fellow travelers in the customer journey. Gone are the days of conventional marketing solely driven by generating sales. We're in an environment that puts building relationships and serving those who share their passion to mutual benefit, rather than an exploitative, predatory one, one that's solely interested in increasing the bottom line by any means necessary.
-        <br /><br />
+        Marketing is no longer about drawing attention in a saturated market; rather, it is about
+        fostering dialog with fellow travelers in the customer journey. Gone are the days of
+        conventional marketing solely driven by generating sales. We're in an environment that puts
+        building relationships and serving those who share their passion to mutual benefit, rather
+        than an exploitative, predatory one, one that's solely interested in increasing the bottom
+        line by any means necessary.
+        <br />
+        <br />
         Here are some essential marketing tips for creatives in the space:
-        <br /><br />
+        <br />
+        <br />
         <span>Build a tribe.</span> <br />
-        Seth Godin put it best when he said that people need to find their tribe. Creators do too. There's no way out of it — you need to be out there to find your tribe of followers, friends, fans, and believers who resonate with your worldview. Don't overthink: what matters most is establishing trust. The medium isn’t the constraint.
-        <br /><br />
+        Seth Godin put it best when he said that people need to find their tribe. Creators do too.
+        There's no way out of it — you need to be out there to find your tribe of followers,
+        friends, fans, and believers who resonate with your worldview. Don't overthink: what matters
+        most is establishing trust. The medium isn’t the constraint.
+        <br />
+        <br />
         <span>Engage the Community.</span> <br />
-        Community is the backbone of crypto, and NFTs aren't exempted. Therefore building one — your tribe, as it were — is of utmost importance. These are the individuals who invest in you as a creator, spread the word about you, and buy or help sell your NFTs. Social media is your friend: Twitter, Telegram, Discord, Reddit, Medium, and LinkedIn are all critical promotional platforms that engage individuals in the space, educate normies about NFTs and digital art, and receive valuable feedback across all audiences. Dialog is crucial more than ever, and these tools will help your harness its power to provide value to the community and establish authority and trust.
-        <br /><br />
-        <span>Get listed on marketplaces.</span><br />
-        OpenSea, Rarible, Foundation and SuperRare are among the most common NFT marketplaces to sell your ideas and artwork. Each has its unique features, benefits, and drawbacks, so always read through the fine print of how they work and if it fits your project. 
+        Community is the backbone of crypto, and NFTs aren't exempted. Therefore building one — your
+        tribe, as it were — is of utmost importance. These are the individuals who invest in you as
+        a creator, spread the word about you, and buy or help sell your NFTs. Social media is your
+        friend: Twitter, Telegram, Discord, Reddit, Medium, and LinkedIn are all critical
+        promotional platforms that engage individuals in the space, educate normies about NFTs and
+        digital art, and receive valuable feedback across all audiences. Dialog is crucial more than
+        ever, and these tools will help your harness its power to provide value to the community and
+        establish authority and trust.
+        <br />
+        <br />
+        <span>Get listed on marketplaces.</span>
+        <br />
+        OpenSea, Rarible, Foundation and SuperRare are among the most common NFT marketplaces to
+        sell your ideas and artwork. Each has its unique features, benefits, and drawbacks, so
+        always read through the fine print of how they work and if it fits your project.
       </p>
       <p className={styles.text3}>
-        <span>Hit Discord and Telegram.</span> <br />
-        Discord and Telegram are established communication channels for crypto and NFTs — and the relationship between the latter with gaming is inseparable. The gaming community is inextricably linked to NFTs, which is why Discord is a prerequisite to finding the audience your artwork needs.
-        <br /><br />
-        <span>Public relations.</span> <br />
-        Once the word gets around, it's essential for you as an artist to get into the public relations side of things, such as getting mentions, quotes, and articles about your project in crypto and NFT media. You know what they say — all publicity is good publicity. 
-        <br /><br />
-        <span>Developing your 'inner game'</span><br />
-        However, as more creators enter the digital art and NFT scene, a reckoning is in order. As the furor dies down, the debate around the interwebs is shifting towards how new and aspiring artists in this space can handle the peaks and troughs of a technology, industry, and market that's constantly evolving by the day — which is something they might not be prepared to deal with.
-        <br /><br />
-        Artists are a progressive and ethical bunch. And the insane amounts of money, fame, and success from a bullish NFT market — or the lack thereof — have become a source of conflicting emotions and abject mental exhaustion.
-        <br /><br />
-        While the NFT community's spirit of positivity and encouragement has brought in creators from every corner of the world, there comes the point where all the positive vibes become trite, platitudes, or pandering motherhood statements. And it can be toxic for everyone in the space, pros or average joes notwithstanding.
-        <br /><br />
-        Therefore, don't forget to develop your inner game as you set out in the world of crypto art. 
-
-
-      </p>
-      <p className={styles.text4}>
-        <span>Raising Your Marketing Game</span><br />
-        We've all heard the "starving artist" trope. We've also heard the stereotypical assertion that artists aren't generally renowned marketers. But in this frenzied environment that art finds itself in, artists can only survive by marketing their works of art. Therefore, today's digital creators need an adequate marketing strategy to give themselves the best chance to succeed.
-        <br /><br />
-        Marketing is no longer about drawing attention in a saturated market; rather, it is about fostering dialog with fellow travelers in the customer journey. Gone are the days of conventional marketing solely driven by generating sales. We're in an environment that puts building relationships and serving those who share their passion to mutual benefit, rather than an exploitative, predatory one, one that's solely interested in increasing the bottom line by any means necessary.
+        Hit Discord and Telegram.
+        <br />
+        Discord and Telegram are established communication channels for crypto and NFTs — and the
+        relationship between the latter with gaming is inseparable. The gaming community is
+        inextricably linked to NFTs, which is why Discord is a prerequisite to finding the audience
+        your artwork needs.
+        <br />
+        <br />
+        Public relations.
+        <br />
+        Once the word gets around, it's essential for you as an artist to get into the public
+        relations side of things, such as getting mentions, quotes, and articles about your project
+        in crypto and NFT media. You know what they say — all publicity is good publicity.
+        <br />
+        <br />
+        Developing your 'inner game'
+        <br />
+        However, as more creators enter the digital art and NFT scene, a reckoning is in order. As
+        the furor dies down, the debate around the interwebs is shifting towards how new and
+        aspiring artists in this space can handle the peaks and troughs of a technology, industry,
+        and market that's constantly evolving by the day — which is something they might not be
+        prepared to deal with.
+        <br />
+        <br />
+        Artists are a progressive and ethical bunch. And the insane amounts of money, fame, and
+        success from a bullish NFT market — or the lack thereof — have become a source of
+        conflicting emotions and abject mental exhaustion.
+        <br />
+        <br />
+        While the NFT community's spirit of positivity and encouragement has brought in creators
+        from every corner of the world, there comes the point where all the positive vibes become
+        trite, platitudes, or pandering motherhood statements. And it can be toxic for everyone in
+        the space, pros or average joes notwithstanding.
+        <br />
+        <br />
+        Therefore, don't forget to develop your inner game as you set out in the world of crypto
+        art.
       </p>
     </div>
   );
