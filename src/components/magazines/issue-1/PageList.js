a09eecd62548af0edba13a96a732a16eac16b6ee
wj-code-mania <wonyy1988@outlook.com>
2021-05-14 13:03:26 -0700

    feat: add loadable component

--- src/components/magazines/issue-1/PageList.js
+++ src/components/magazines/issue-1/PageList.js
@@ -1,55 +1,111 @@
-import CoverPage from './CoverPage'
-import Page12 from './Page12'
-import Page34 from './Page34'
-import Page56 from './Page56'
-import Page78 from './Page78'
-import Page910 from './Page910'
-import Page1112 from './Page1112'
-import Page1314 from './Page1314'
-import Page1516 from './Page1516'
-import Page1718 from './Page1718'
-import Page1920 from './Page1920'
-import Page2122 from './Page2122'
-import Page2324 from './Page2324'
-import Page2526 from './Page2526'
-import Page2728 from './Page2728'
-import Page2930 from './Page2930'
-import Page3132 from './Page3132'
-import Page3334 from './Page3334'
-import Page3536 from './Page3536'
-import Page3738 from './Page3738'
-import Page3940 from './Page3940'
-import Page4142 from './Page4142'
-import Page4344 from './Page4344'
-import Page4546 from './Page4546'
-import Page4748 from './Page4748'
-import Page4950 from './Page4950'
-import Page5152 from './Page5152'
-import Page5354 from './Page5354'
-import Page5556 from './Page5556'
-import Page5758 from './Page5758'
-import Page5960 from './Page5960'
-import Page6162 from './Page6162'
-import Page6364 from './Page6364'
-import Page6566 from './Page6566'
-import Page6768 from './Page6768'
-import Page6970 from './Page6970'
-import Page7172 from './Page7172'
-import Page7374 from './Page7374'
-import Page7576 from './Page7576'
-import Page7778 from './Page7778'
-import Page7980 from './Page7980'
-import Page8182 from './Page8182'
-import Page8384 from './Page8384'
-import Page8586 from './Page8586'
-import Page8788 from './Page8788'
+import loadable from '@loadable/component'
+const CoverPage = loadable(() => import ('./CoverPage'))
+const Page12 = loadable(() => import ('./Page12'))
+const Page34 = loadable(() => import ('./Page34'))
+const Page56 = loadable(() => import ('./Page56'))
+const Page78 = loadable(() => import ('./Page78'))
+const Page910 = loadable(() => import ('./Page910'))
+const Page1112 = loadable(() => import ('./Page1112'))
+const Page1314 = loadable(() => import ('./Page1314'))
+const Page1516 = loadable(() => import ('./Page1516'))
+const Page1718 = loadable(() => import ('./Page1718'))
+const Page1920 = loadable(() => import ('./Page1920'))
+const Page2122 = loadable(() => import ('./Page2122'))
+const Page2324 = loadable(() => import ('./Page2324'))
+const Page2526 = loadable(() => import ('./Page2526'))
+const Page2728 = loadable(() => import ('./Page2728'))
+const Page2930 = loadable(() => import ('./Page2930'))
+const Page3132 = loadable(() => import ('./Page3132'))
+const Page3334 = loadable(() => import ('./Page3334'))
+const Page3536 = loadable(() => import ('./Page3536'))
+const Page3738 = loadable(() => import ('./Page3738'))
+const Page3940 = loadable(() => import ('./Page3940'))
+
+const Page4142 = loadable(() => import ('./Page4142'))
+const Page4344 = loadable(() => import ('./Page4344'))
+const Page4546 = loadable(() => import ('./Page4546'))
+const Page4748 = loadable(() => import ('./Page4748'))
+const Page4950 = loadable(() => import ('./Page4950'))
+const Page5152 = loadable(() => import ('./Page5152'))
+const Page5354 = loadable(() => import ('./Page5354'))
+const Page5556 = loadable(() => import ('./Page5556'))
+const Page5758 = loadable(() => import ('./Page5758'))
+const Page5960 = loadable(() => import ('./Page5960'))
+const Page6162 = loadable(() => import ('./Page6162'))
+const Page6364 = loadable(() => import ('./Page6364'))
+const Page6566 = loadable(() => import ('./Page6566'))
+const Page6768 = loadable(() => import ('./Page6768'))
+const Page6970 = loadable(() => import ('./Page6970'))
+const Page7172 = loadable(() => import ('./Page7172'))
+const Page7374 = loadable(() => import ('./Page7374'))
+const Page7576 = loadable(() => import ('./Page7576'))
+const Page7778 = loadable(() => import ('./Page7778'))
+const Page7980 = loadable(() => import ('./Page7980'))
+const Page8182 = loadable(() => import ('./Page8182'))
+const Page8384 = loadable(() => import ('./Page8384'))
+const Page8586 = loadable(() => import ('./Page8586'))
+const Page8788 = loadable(() => import ('./Page8788'))
 // Hidden Pages
-import Page8990 from './Page8990'
-import Page9192 from './Page9192'
-import Page9394 from './Page9394'
-import Page9596 from './Page9596'
-import Page9798 from './Page9798'
-import Backcover from './Backcover'
+const Page8990 = loadable(() => import ('./Page8990'))
+const Page9192 = loadable(() => import ('./Page9192'))
+const Page9394 = loadable(() => import ('./Page9394'))
+const Page9596 = loadable(() => import ('./Page9596'))
+const Page9798 = loadable(() => import ('./Page9798'))
+
+const Backcover = loadable(() => import ('./Backcover'))
+
+// import CoverPage from './CoverPage'
+// import Page12 from './Page12'
+// import Page34 from './Page34'
+// import Page56 from './Page56'
+// import Page78 from './Page78'
+// import Page910 from './Page910'
+// import Page1112 from './Page1112'
+// import Page1314 from './Page1314'
+// import Page1516 from './Page1516'
+// import Page1718 from './Page1718'
+// import Page1920 from './Page1920'
+// import Page2122 from './Page2122'
+// import Page2324 from './Page2324'
+// import Page2526 from './Page2526'
+// import Page2728 from './Page2728'
+// import Page2930 from './Page2930'
+// import Page3132 from './Page3132'
+// import Page3334 from './Page3334'
+// import Page3536 from './Page3536'
+// import Page3738 from './Page3738'
+// import Page3940 from './Page3940'
+// import Page4142 from './Page4142'
+// import Page4344 from './Page4344'
+// import Page4546 from './Page4546'
+// import Page4748 from './Page4748'
+// import Page4950 from './Page4950'
+// import Page5152 from './Page5152'
+// import Page5354 from './Page5354'
+// import Page5556 from './Page5556'
+// import Page5758 from './Page5758'
+// import Page5960 from './Page5960'
+// import Page6162 from './Page6162'
+// import Page6364 from './Page6364'
+// import Page6566 from './Page6566'
+// import Page6768 from './Page6768'
+// import Page6970 from './Page6970'
+// import Page7172 from './Page7172'
+// import Page7374 from './Page7374'
+// import Page7576 from './Page7576'
+// import Page7778 from './Page7778'
+// import Page7980 from './Page7980'
+// import Page8182 from './Page8182'
+// import Page8384 from './Page8384'
+// import Page8586 from './Page8586'
+// import Page8788 from './Page8788'
+// Hidden Pages
+// import Page8990 from './Page8990'
+// import Page9192 from './Page9192'
+// import Page9394 from './Page9394'
+// import Page9596 from './Page9596'
+// import Page9798 from './Page9798'
+// import Backcover from './Backcover'
 
 const PageList = [
     <CoverPage />,
