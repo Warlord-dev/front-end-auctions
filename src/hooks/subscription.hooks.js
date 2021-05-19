f40ac3b1bef37f36336f08ecedec0d19d6f6c16e
CameronS29 <c.sato.dev@gmail.com>
2021-05-19 19:49:57 -0400

    DIG-132: subscription hook updated.

--- src/hooks/subscription.hooks.js
+++ src/hooks/subscription.hooks.js
@@ -2,16 +2,19 @@ import { useEffect } from 'react';
 
 export const useSubscription = ({ request, next }, deps) => {
   useEffect(() => {
-    const { unsubscribe } = request.subscribe({
-      next: ({ data }) => {
-        if (!data) {
-          return;
-        }
-
-        next(data);
-      },
-    });
-
-    return () => unsubscribe();
+    if (deps) {
+      console.log('this is request subscribe send');
+      const { unsubscribe } = request.subscribe({
+        next: ({ data }) => {
+          if (!data) {
+            return;
+          }
+  
+          next(data);
+        },
+      });
+  
+      return () => unsubscribe();
+    }
   }, deps);
 };
