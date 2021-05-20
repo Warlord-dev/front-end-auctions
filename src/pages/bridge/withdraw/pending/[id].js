24cba9b210e4bfd5266136b9065cf8490aeefb0e
jupiteruwdev <harutoinoue8@gmail.com>
2021-05-20 10:11:24 -0400

    DIG-118: pending withdraw table updated.

--- src/pages/bridge/withdraw/pending/[id].js
+++ src/pages/bridge/withdraw/pending/[id].js
@@ -1,14 +1,22 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Link from 'next/link';
 import BridgeModal from '@components/bridge-modal';
 import PendingTable from '@components/pending-table';
-import Link from 'next/link';
-import React from 'react';
+import { getUser } from '@helpers/user.helpers';
 import styles from './styles.module.scss';
+import { useRouter } from 'next/router';
 
 const Pending = () => {
+  const router = useRouter();
+  const id = parseInt(router.query.id);
+  const user = useSelector(getUser);
+  const pendingWithdrawals = user.withdrawalTxs.filter((tx) => tx.status === 'pending');
+
   return (
     <div className={styles.wrapper}>
       <BridgeModal title="Pending Withdrawals" mode={1}>
-        <PendingTable data={{}} mode={1} />
+        <PendingTable data={pendingWithdrawals} id={id} mode={1} />
         <hr />
         <div className={styles.actions}>
           <Link href="/bridge">
