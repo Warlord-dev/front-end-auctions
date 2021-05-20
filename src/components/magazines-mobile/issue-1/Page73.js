ceb8228ffc85b28bdb761fd1025a623097a6380f
wj-code-mania <wonyy1988@outlook.com>
2021-05-19 23:07:03 -0700

    feat: added template pages

---
+++ src/components/magazines-mobile/issue-1/Page73.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './page67.module.scss'
+
+const Page67 = () => {
+  return (
+    <div className={styles.wrapper}>
+     
+      
+    </div>
+  )
+}
+
+export default Page67
--- src/components/magazines-mobile/issue-1/PageList.js
+++ src/components/magazines-mobile/issue-1/PageList.js
@@ -26,12 +26,68 @@ const Page23 = loadable(() => import ('./Page23'))
 const Page24 = loadable(() => import ('./Page24'))
 const Page25 = loadable(() => import ('./Page25'))
 const Page26 = loadable(() => import ('./Page26'))
-const Page27 = loadable(() => import ('./Page32'))
-const Page28 = loadable(() => import ('./Page27'))
-const Page29 = loadable(() => import ('./Page28'))
-const Page30 = loadable(() => import ('./Page29'))
-const Page31 = loadable(() => import ('./Page30'))
-const Page32 = loadable(() => import ('./Page31'))
+const Page27 = loadable(() => import ('./Page27'))
+const Page28 = loadable(() => import ('./Page28'))
+const Page29 = loadable(() => import ('./Page29'))
+const Page30 = loadable(() => import ('./Page30'))
+const Page31 = loadable(() => import ('./Page31'))
+const Page32 = loadable(() => import ('./Page32'))
+const Page33 = loadable(() => import ('./Page33'))
+const Page34 = loadable(() => import ('./Page34'))
+const Page35 = loadable(() => import ('./Page35'))
+const Page36 = loadable(() => import ('./Page36'))
+const Page37 = loadable(() => import ('./Page37'))
+const Page38 = loadable(() => import ('./Page38'))
+const Page39 = loadable(() => import ('./Page39'))
+const Page40 = loadable(() => import ('./Page40'))
+const Page41 = loadable(() => import ('./Page41'))
+const Page42 = loadable(() => import ('./Page42'))
+const Page43 = loadable(() => import ('./Page43'))
+const Page44 = loadable(() => import ('./Page44'))
+const Page45 = loadable(() => import ('./Page45'))
+const Page46 = loadable(() => import ('./Page46'))
+const Page47 = loadable(() => import ('./Page47'))
+const Page48 = loadable(() => import ('./Page48'))
+const Page49 = loadable(() => import ('./Page49'))
+const Page50 = loadable(() => import ('./Page50'))
+const Page51 = loadable(() => import ('./Page51'))
+const Page52 = loadable(() => import ('./Page52'))
+const Page53 = loadable(() => import ('./Page53'))
+const Page54 = loadable(() => import ('./Page54'))
+const Page55 = loadable(() => import ('./Page55'))
+const Page56 = loadable(() => import ('./Page56'))
+const Page57 = loadable(() => import ('./Page57'))
+const Page58 = loadable(() => import ('./Page58'))
+const Page59 = loadable(() => import ('./Page59'))
+const Page60 = loadable(() => import ('./Page60'))
+const Page61 = loadable(() => import ('./Page61'))
+const Page62 = loadable(() => import ('./Page62'))
+const Page63 = loadable(() => import ('./Page63'))
+const Page64 = loadable(() => import ('./Page64'))
+const Page65 = loadable(() => import ('./Page65'))
+const Page66 = loadable(() => import ('./Page66'))
+const Page67 = loadable(() => import ('./Page67'))
+const Page68 = loadable(() => import ('./Page68'))
+const Page69 = loadable(() => import ('./Page69'))
+const Page70 = loadable(() => import ('./Page70'))
+const Page71 = loadable(() => import ('./Page71'))
+const Page72 = loadable(() => import ('./Page72'))
+const Page73 = loadable(() => import ('./Page73'))
+const Page74 = loadable(() => import ('./Page74'))
+const Page75 = loadable(() => import ('./Page75'))
+const Page76 = loadable(() => import ('./Page76'))
+const Page77 = loadable(() => import ('./Page77'))
+const Page78 = loadable(() => import ('./Page78'))
+const Page79 = loadable(() => import ('./Page79'))
+const Page80 = loadable(() => import ('./Page80'))
+const Page81 = loadable(() => import ('./Page81'))
+const Page82 = loadable(() => import ('./Page82'))
+const Page83 = loadable(() => import ('./Page83'))
+const Page84 = loadable(() => import ('./Page84'))
+const Page85 = loadable(() => import ('./Page85'))
+const Page86 = loadable(() => import ('./Page86'))
+const Page87 = loadable(() => import ('./Page87'))
+const Page88 = loadable(() => import ('./Page88'))
 
 const Backcover = loadable(() => import ('./Backcover'))
 
@@ -69,6 +125,62 @@ const PageList = [
   <Page30 />,
   <Page31 />,
   <Page32 />,
+  <Page33 />,
+  <Page34 />,
+  <Page35 />,
+  <Page36 />,
+  <Page37 />,
+  <Page38 />,
+  <Page39 />,
+  <Page40 />,
+  <Page41 />,
+  <Page42 />,
+  <Page43 />,
+  <Page44 />,
+  <Page45 />,
+  <Page46 />,
+  <Page47 />,
+  <Page48 />,
+  <Page49 />,
+  <Page50 />,
+  <Page51 />,
+  <Page52 />,
+  <Page53 />,
+  <Page54 />,
+  <Page55 />,
+  <Page56 />,
+  <Page57 />,
+  <Page58 />,
+  <Page59 />,
+  <Page60 />,
+  <Page61 />,
+  <Page62 />,
+  <Page63 />,
+  <Page64 />,
+  <Page65 />,
+  <Page66 />,
+  <Page67 />,
+  <Page68 />,
+  <Page69 />,
+  <Page70 />,
+  <Page71 />,
+  <Page72 />,
+  <Page73 />,
+  <Page74 />,
+  <Page75 />,
+  <Page76 />,
+  <Page77 />,
+  <Page78 />,
+  <Page79 />,
+  <Page80 />,
+  <Page81 />,
+  <Page82 />,
+  <Page83 />,
+  <Page84 />,
+  <Page85 />,
+  <Page86 />,
+  <Page87 />,
+  <Page88 />,
   <Backcover />
 ]
 
