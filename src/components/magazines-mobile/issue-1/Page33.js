1fcc4c1ad68428e7e756abedcb133f8f108a056d
wj-code-mania <wonyy1988@outlook.com>
2021-05-19 23:44:25 -0700

    fix: page 33

--- src/components/magazines-mobile/issue-1/Page33.js
+++ src/components/magazines-mobile/issue-1/Page33.js
@@ -5,43 +5,114 @@ const Page33 = () => {
   return (
     <div className={styles.wrapper}>
       <img src="./magazine/1/images/mobile/page39-bg-1.png" className={styles.backImage} />
-      <div className={styles.innerWrapper}>
-        <div className={styles.section1}>
-          <h1>ESPA</h1>
-          <h2>CASUAL ESPORTS </h2>          
-          <p>ESPA is the first indie and modded eesports platform. Established to liberate the gaming industry, allowing any player, dev, designer to go from amateur to pro and level up through web3 based casual esports.</p>
-          <p>it started with among us mods and now we are moving onto minecraft! espa will be launching mc casual esports battles this month. players wear unique erc-721 nft skins in-game to be authenticated for winning streaks.</p>
-          <p>check out the unlockable nft subscriptions for an espa compatible mc nft skin! </p>
-          <img src="./magazine/1/images/mobile/lego.png" />
+      <h1>ESPA</h1>
+      <h2>CASUAL ESPORTS </h2>          
+      <p className={styles.text1}>
+        ESPA is the first indie and modded eesports platform. Established to liberate the gaming industry, allowing any player, dev, designer to go from amateur to pro and level up through web3 based casual esports.
+        <br /><br />
+        it started with among us mods and now we are moving onto minecraft! espa will be launching mc casual esports battles this month. players wear unique erc-721 nft skins in-game to be authenticated for winning streaks.
+        <br /><br />
+        check out the unlockable nft subscriptions for an espa compatible mc nft skin!         
+      </p>
+      <video muted autoPlay loop className={styles.video1}>
+        <source src="./magazine/1/39_40/Untitled (20).mp4" type="video/mp4" />
+      </video>
+      <img src="./magazine/1/images/mobile/arrow-top.png" className={styles.arrowTop} />
+      <p className={styles.text2}>
+        SKINS BY CLEORA AND ANIMATION BY DIGITALAX IN-HOUSE FASHION DESIGN STUDIO
+      </p>
+      <p className={styles.text3}>
+        eRC-721 NFT THAT CAN BE TAKEN INTO MineCraft FOR SCORING WINNING STREAKS IN THE ESPA TOURNAMENTS AND EARNING $MONA CRYPTO INCOME STREAMS. 
+      </p>
+      <h3>UNLOCKABLE MC NFT SKIN! 3 DEGREES OF RARITY! </h3>
+      <img src="./magazine/1/images/mobile/arrow-down.png" className={styles.arrowBottom} />
+      <h4>MINECRAFT ESPORTS with ESPA</h4>
+      {/* <img src="./magazine/1/images/mobile/image 316.png" />
+      <img src="./magazine/1/images/mobile/2021-04-27_12.56.png" />
+      <img src="./magazine/1/images/mobile/image 314.png" />
+      <img src="./magazine/1/images/mobile/image 315.png" /> */}
+      <div className={styles.animationFrame}>
+        <div>
+          <img src="./magazine/1/39_40/image 316 (1).png" className={styles.image1} />
+          <img src="./magazine/1/39_40/2021-04-27_12.56.png" className={styles.image2} />
+          <img src="./magazine/1/39_40/2021-04-27_12.19.png" className={styles.image3} />
         </div>
-        <div className={styles.section2}>
-          <img src="./magazine/1/images/mobile/arrow-top.png" className={styles.arrowTop} />
-          <h3>SKINS BY CLEORA AND ANIMATION BY DIGITALAX IN-HOUSE FASHION DESIGN STUDIO </h3>
-          <h4>eRC-721 NFT THAT CAN BE TAKEN INTO MineCraft FOR SCORING WINNING STREAKS IN THE ESPA TOURNAMENTS AND EARNING $MONA CRYPTO INCOME STREAMS. </h4>
-          <h5>UNLOCKABLE MC NFT SKIN! 3 DEGREES OF RARITY! </h5>
-          <img src="./magazine/1/images/mobile/arrow-down.png" className={styles.arrowBottom} />
-          <h1>MINECRAFT ESPORTS with ESPA</h1>
+        <div>
+          <img src="./magazine/1/39_40/image 316 (1).png" className={styles.image1} />
+          <img src="./magazine/1/39_40/2021-04-27_12.56.png" className={styles.image2} />
+          <img src="./magazine/1/39_40/2021-04-27_12.19.png" className={styles.image3} />
         </div>
-        <div className={styles.section3}>
-          <img src="./magazine/1/images/mobile/image 316.png" />
-          <img src="./magazine/1/images/mobile/2021-04-27_12.56.png" />
+        <div>
+          <img src="./magazine/1/39_40/image 316 (1).png" className={styles.image1} />
+          <img src="./magazine/1/39_40/2021-04-27_12.56.png" className={styles.image2} />
+          <img src="./magazine/1/39_40/2021-04-27_12.19.png" className={styles.image3} />
         </div>
-        <div className={styles.section4}>
-          <img src="./magazine/1/images/mobile/image 314.png" />
-          <img src="./magazine/1/images/mobile/image 315.png" />
+        <div>
+          <img src="./magazine/1/39_40/image 316 (1).png" className={styles.image1} />
+          <img src="./magazine/1/39_40/2021-04-27_12.56.png" className={styles.image2} />
+          <img src="./magazine/1/39_40/2021-04-27_12.19.png" className={styles.image3} />
         </div>
-        <div className={styles.section5}>
-          <img src="./magazine/1/images/mobile/india 2.png" className={styles.india_2}/>
-          <h1>MINECRAFT</h1>
-          <h1>INDIA</h1>
-          <h2>INMC</h2>
-          <p>BEDWARS.</p>
-          <p>SKYWARS.DUELS.</p>
-          <p>SURVIVAL.FACTIONS.</p>
-          <p>SKYBLOCK.</p>
-        </div>
-        
       </div>
+      <div className={styles.animationFrame1}>
+        <div>
+          <img src="./magazine/1/39_40/image 314 (1).png" className={styles.image1} />
+          <img src="./magazine/1/39_40/image 315.png" className={styles.image2} />
+          <img src="./magazine/1/39_40/unknown8.png" className={styles.image3} />
+        </div>
+        <div>
+          <img src="./magazine/1/39_40/image 314 (1).png" className={styles.image1} />
+          <img src="./magazine/1/39_40/image 315.png" className={styles.image2} />
+          <img src="./magazine/1/39_40/unknown8.png" className={styles.image3} />
+        </div>
+        <div>
+          <img src="./magazine/1/39_40/image 314 (1).png" className={styles.image1} />
+          <img src="./magazine/1/39_40/image 315.png" className={styles.image2} />
+          <img src="./magazine/1/39_40/unknown8.png" className={styles.image3} />
+        </div>
+        <div>
+          <img src="./magazine/1/39_40/image 314 (1).png" className={styles.image1} />
+          <img src="./magazine/1/39_40/image 315.png" className={styles.image2} />
+          <img src="./magazine/1/39_40/unknown8.png" className={styles.image3} />
+        </div>
+      </div>
+      <div className={styles.animationFrame2}>
+        <div>
+          <img src="./magazine/1/39_40/unknown3.png" className={styles.image1} />
+          <img src="./magazine/1/39_40/image 317.png" className={styles.image2} />
+          <img src="./magazine/1/39_40/image 319 (1).png" className={styles.image3} />
+        </div>
+        <div>
+          <img src="./magazine/1/39_40/unknown3.png" className={styles.image1} />
+          <img src="./magazine/1/39_40/image 317.png" className={styles.image2} />
+          <img src="./magazine/1/39_40/image 319 (1).png" className={styles.image3} />
+        </div>
+        <div>
+          <img src="./magazine/1/39_40/unknown3.png" className={styles.image1} />
+          <img src="./magazine/1/39_40/image 317.png" className={styles.image2} />
+          <img src="./magazine/1/39_40/image 319 (1).png" className={styles.image3} />
+        </div>
+        <div>
+          <img src="./magazine/1/39_40/unknown3.png" className={styles.image1} />
+          <img src="./magazine/1/39_40/image 317.png" className={styles.image2} />
+          <img src="./magazine/1/39_40/image 319 (1).png" className={styles.image3} />
+        </div>
+      </div>
+  
+      <img src="./magazine/1/images/mobile/india 2.png" className={styles.india_2}/>
+      <h5>
+        MINECRAFT <br />
+        INDIA
+      </h5>
+      <h6>
+        INMC
+      </h6>
+      
+      <p className={styles.text4}>BEDWARS.</p>
+      <p className={styles.text5}>SKYWARS.DUELS.</p>
+      <p className={styles.text6}>SURVIVAL.FACTIONS.</p>
+      <p className={styles.text7}>SKYBLOCK.</p>
+   
+        
     </div>
   )
 }
