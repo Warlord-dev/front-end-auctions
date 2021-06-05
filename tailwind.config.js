b69bbb35fada4cd6d4ca6587c897f1b08b6fee57
Okawa <okawanori001@gmail.com>
2021-06-05 15:10:49 -0400

    Fix tailwand config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 // tailwind.config.js
 module.exports = {
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
