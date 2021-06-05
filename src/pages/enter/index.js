32bb150b69cfb01942321912136fdc555704391e
Okawa <okawanori001@gmail.com>
2021-06-05 14:25:45 -0400

    New Design

--- src/pages/enter/index.js
+++ src/pages/enter/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Modal from '../../components/Modal';
+// import Modal from '../../components/Modal';
 
 function Home(props) {
   const [open, setOpen] = useState(false);
