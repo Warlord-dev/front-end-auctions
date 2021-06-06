809d582baa4fdcb05dda7f3a33e5ba1a6871f682
slarkjm0803 <slarkjm@outlook.com>
2021-06-07 03:13:52 +0900

    fix:first commit

--- src/components/magazines/issue-2/Page8788.js
+++ src/components/magazines/issue-2/Page8788.js
@@ -2,50 +2,57 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import UnlockPage from '../common/UnlockPage';
 import styles from './page8788.module.scss';
+import cn from 'classnames';
 
 const Page8788 = () => {
   const { contentUnlocked } = useSelector((state) => state.global.toJS());
 
   return (
     <div className={styles.wrapper}>
-      <img src="./magazine/1/87_88/pINT 2.png" className={styles.line} />
-      <img src="./magazine/1/images/last-page.jpg" className={styles.rightBack} />
-      <div className={styles.mainTitle}>Mod the Metaverse.</div>
-      <div className={styles.text}>
-        DIGIFIZZY is for the dreamers, the creators, the players, the technologists, the developers,
-        the risk takers. It’s for anyone out there that truly believes in the potentiality of where
-        we are headed. It’s for those that want to be part of building the right metaverse,
-        underwriting it with the value and ethos that we keep being promised, but is yet to actually
-        play out.
-        <br />
-        <br />
-        We are all climbing a mountain that has never been climbed before. Charting a new course.
-        There is no textbook or pre-prepared answers for what is to come as we traverse these new
-        plains. But ultimately, there doesn’t need to be. Because we all already know the map that
-        we want to navigate. It’s one that ensures openness, vitality and allows us the ability to
-        spin up personal decentralised realms with dynamic access keys.
-        <br />
-        <br />
-        It’s one that allows us to mod— to break everything down into all of its parts, tweak it,
-        mess around with it, and then reassemble it into something new, novel, generative— engage in
-        the practice of driving growth, continued betterness. As we build out these new
-        decentralised networks and ecosystems we must make sure that humanity stays at the heart of
-        it. We must take control of our own identities, our own quests, and not be conformed by a
-        legacy frame that does no justice but to make us ponder “Cui bono?”.
-        <br />
-        <br />
-        <b>DIGIFIZZY is for the gatemakers.</b>
-        <br />
-        <br />
-        It’s not just the pursuit for enabling greater potentiality in all of us, but rather,
-        actually making it happen. I invite you all to join us in living this through. That’s all it
-        is. A leap of faith.
-        <br />
-        <br />
-        To be continued.
+      <img src="./magazine/1/87_88/page87-left.png" className={styles.leftBack} />
+      <img src="./magazine/1/87_88/page87-right.png" className={styles.rightBack} />
+      <img src="./magazine/1/87_88/page87-right2.png" className={styles.rightBack} />
+      <img src="./magazine/1/87_88/page87-right3.png" className={styles.rightBottom} />
+      <div className={cn(styles.text, styles.text1)}>
+      WHAT ARE YOU WEARING?
       </div>
-      <div className={styles.bottomText}>- Emma-Jane MacKinnon-Lee</div>
-      {!contentUnlocked && <UnlockPage />}
+      <div className={cn(styles.text, styles.text2)}>
+      WHAT ARE YOU WEARING?
+      </div>
+      <div className={cn(styles.text, styles.text3)}>
+      WHAT ARE YOU WEARING?
+      </div>
+      <div className={cn(styles.text, styles.text4)}>
+      WHAT ARE YOU WEARING?
+      </div>
+      <div className={cn(styles.text, styles.text5)}>
+      WHAT ARE YOU WEARING?
+      </div>
+      <div className={styles.text6}>
+      FASHION FOR THE METAVERSE
+      </div>
+      <div className={styles.text7}>
+      This month DIGITALAX ran...
+      </div>
+      <div className={styles.text8}>
+      #FLEXYOURFLARE
+      </div>
+      <div className={styles.text9}>
+      #FLEXYOURFLARE
+      </div>
+      <div className={styles.text10}>
+      #FLEXYOURFLARE
+      </div>
+      <div className={styles.text11}>
+      An open digital fashion competition bringing together amazingly talented creators from across the realms, focused on enabling creatives for modding their version of the metaverse.
+      </div>
+      <div className={styles.text12}>
+      This might sound cliche but all entries were too good not to include in DIGIFIZZY. We truly felt that everyone’s efforts deserved to be seen! 
+      </div>
+      <div className={styles.text13}>
+      So, for the next pages of What Are you Wearing? feast your eyes on some delightful digital fashion visual salience. 
+      </div>
+      {/* {!contentUnlocked && <UnlockPage />} */}
     </div>
   );
 };
