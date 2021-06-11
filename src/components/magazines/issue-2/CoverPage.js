81f5ae675c40a29776add69a620bf2bfd27985ef
jupiteruwdev <harutoinoue8@gmail.com>
2021-06-11 10:57:57 -0500

    issue 2 cover page added.

--- src/components/magazines/issue-2/CoverPage.js
+++ src/components/magazines/issue-2/CoverPage.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 import React from 'react';
 import styles from './coverpage.module.scss';
-=======
-import React from 'react'
-import styles from './coverpage.module.scss'
->>>>>>> ad593b7cd2b4628039bd8fd6752565b828751313
 
 const CoverPage = () => {
   return (
     <div className={styles.wrapper}>
-<<<<<<< HEAD
       <img src="/magazine/2/images/home.png" width="960" />
       <div className={styles.deco}>
         DECO <br /> <br /> ONLY <br />
@@ -46,48 +40,3 @@ const CoverPage = () => {
 };
 
 export default CoverPage;
-=======
-      <img src='/magazine/1/images/cover.jpg' width='960'/>
-      <h1>
-        I D & E G 0
-      </h1>
-      <h2 className={styles.india}>
-        I N D I A
-      </h2>
-      <h2 className={styles.forweb3}>
-        FOR WEB3
-      </h2>
-      <h3>
-        The Next<br/>
-        Real Estate Rush
-      </h3>
-      <h4>
-        Onboarding the 99%        
-      </h4>
-      <h5>
-        METAHUMANISM
-      </h5>
-      <h6>
-        XR Couture
-      </h6>
-      <div className={styles.digifizzy1}>
-        D I G I F I Z Z Y
-      </div>
-      <div className={styles.digifizzy2}>
-        D I G I F I Z Z Y
-      </div>
-      <div className={styles.digifizzy3}>
-        D I G I F I Z Z Y
-      </div>
-      <div className={styles.issue1}>
-        Issue one
-      </div>
-      <div className={styles.issue2}>
-        Issue one
-      </div>
-    </div>
-  )
-}
-
-export default CoverPage
->>>>>>> ad593b7cd2b4628039bd8fd6752565b828751313
