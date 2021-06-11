2199bf6e5e2ec5de921b781f93632019f875feb6
slarkjm0803 <slarkjm@outlook.com>
2021-06-11 11:23:43 +0900

    fix:2 pages fixed

--- src/components/magazines/issue-2/Page111112.js
+++ src/components/magazines/issue-2/Page111112.js
@@ -5,8 +5,8 @@ import Link from 'next/link';
 const Page111112 = () => {
   return (
     <div className={styles.wrapper}>
-       <img src="./magazine/1/107_108/image1.png" className={styles.image1} />
-       <img src="./magazine/1/107_108/image2.png" className={styles.image2} />
+       <img src="./magazine/1/111_112/image1.png" className={styles.image1} />
+       <img src="./magazine/1/111_112/image2.png" className={styles.image2} />
        <p className={styles.text1}>NIWT GNIHSINAV</p>
        <p className={styles.text2}>Off to the Metaverse</p>
        <Link href="https://www.instagram.com/stellaachenbach/"><a className={styles.text3}>
