488910ff0b23e9c666e03f7f66d144dfba58421f
unknown <slarkjm@outlook.com>
2021-07-18 22:13:51 -0700

    fix:updated gif and replaced video

--- src/components/magazines/issue-3/Page12.js
+++ src/components/magazines/issue-3/Page12.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styles from './page12.module.scss';
 const Page12 = () => {
   return (
-    <div className={styles.wrapper}>         
+    <div className={styles.wrapper}>       
+    <img src="/magazine/3/images/01_02/image1.png" className={styles.image1}/>    
     <p className={styles.text1}>Period  | Decentralised Finance refers to financial systems, products and/or services that eliminate centralised authorities, gatekeeping middlemen, and redundant and disposable friction points. DeFi allows anyone with access to web3 to participate as a point of value exchange in a truly person to person network. </p>      
-    <p className={styles.text2}>DEFI</p>
     <p className={styles.text3}>Decentralised Finance</p>
     </div>
   );
