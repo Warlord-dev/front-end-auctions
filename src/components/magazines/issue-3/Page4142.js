a662e72300470d8fbc1f76b212826a86a9825256
CameronS29 <c.sato.dev@gmail.com>
2021-07-20 14:34:07 -0400

    page 37, 42 updated

--- src/components/magazines/issue-3/Page4142.js
+++ src/components/magazines/issue-3/Page4142.js
@@ -19,8 +19,6 @@ const Page4142 = () => {
       <p className={styles.text3}>Designed by DIGITALAX and the Global Designer Network</p>  
       <p className={styles.text8}>PICKLE FINANCE</p>   
       <p className={styles.text10}>RULER PROTOCOL</p>  
-      <p className={styles.text11}>RARI CAPITAL</p>  
-      <p className={styles.text12}>POAP</p>  
       <p className={styles.text13}>FEI PROTOCOL</p>  
       <p className={styles.text14}>POLYGON</p>  
       <p className={styles.text15}>FORCE DAO</p>  
@@ -29,26 +27,24 @@ const Page4142 = () => {
       <img src="/magazine/3/images/39_40/image9.gif" className={styles.image9}/>  
       <img src="/magazine/3/images/39_40/image12.gif" className={styles.image12}/> 
       <img src="/magazine/3/images/39_40/image13.gif" className={styles.image13}/>  
-      <img src="/magazine/3/images/39_40/image14.gif" className={styles.image14}/>
-      <img src="/magazine/3/images/39_40/image15.gif" className={styles.image15}/> 
       <img src="/magazine/3/images/39_40/image16.gif" className={styles.image16}/>  
       <img src="/magazine/3/images/39_40/image17.gif" className={styles.image17}/> 
       <img src="/magazine/3/images/39_40/image18.gif" className={styles.image18}/>
       <img src="/magazine/3/images/39_40/image19.gif" className={styles.image19}/>
-      <img src="/magazine/3/images/39_40/image20.gif" className={styles.image20}/>  
-      <img src="/magazine/3/images/39_40/image21.gif" className={styles.image21}/> 
       <img src="/magazine/3/images/39_40/image22.gif" className={styles.image22}/>  
       <img src="/magazine/3/images/39_40/image23.gif" className={styles.image23}/>  
       <img src="/magazine/3/images/39_40/image24.gif" className={styles.image24}/>  
       <img src="/magazine/3/images/39_40/image25.gif" className={styles.image25}/>  
       <img src="/magazine/3/images/39_40/image13.png" className={styles.image26}/> 
-      <img src="/magazine/3/images/39_40/image11.png" className={styles.image27}/>  
       <img src="/magazine/3/images/39_40/image17.png" className={styles.image32}/>  
-      <img src="/magazine/3/images/39_40/image16.png" className={styles.image33}/>  
       <img src="/magazine/3/images/39_40/image22.png" className={styles.image34}/> 
       <img src="/magazine/3/images/39_40/image18.png" className={styles.image35}/>  
       <img src="/magazine/3/images/39_40/image21.png" className={styles.image36}/>  
       <img src="/magazine/3/images/39_40/image19.png" className={styles.image37}/>  
+      <p className={styles.text18}> GET DEFI DRIP </p>
+      <p className={styles.text17}> GET DEFI DRIP </p>
+      <p className={styles.text19}> Using fashion to onboard millions into DeFi by directly serving their desires.  </p>
+      <img src="/magazine/3/images/39_40/image27.png" className={styles.image41} />
     </div>
   );
 };
