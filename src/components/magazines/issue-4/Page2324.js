4d85e3011e99e116f312aacb1f85d074428336cf
James Lin <wonyy1988@outlook.com>
2021-08-02 12:21:33 -0700

    feat: add page 23,24

--- src/components/magazines/issue-4/Page2324.js
+++ src/components/magazines/issue-4/Page2324.js
@@ -5,7 +5,13 @@ import styles from './page2324.module.scss'
 const Page2324 = () => {
   return (
     <div className={styles.wrapper}>
-      
+      <video autoPlay muted loop className={styles.backVideo}>
+        <source src="./magazine/4/videos/2324_background.mp4" />
+      </video>
+      <div className={styles.text10}>SUSTAINABILITY PIONEERS</div>
+      <img src="./magazine/2/images/31_32_image.png" className={styles.image1} />
+      <div className={styles.middleLine1} />
+      <div className={styles.middleLine2} />
     </div>
   )
 }
