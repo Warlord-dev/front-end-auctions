2f238e0a66a87db3503f6fb977af045774fa6af9
James Lin <wonyy1988@outlook.com>
2021-08-11 12:26:38 -0700

    feat: add 6, 7, 8, 9, 10 for mobile

--- src/components/magazines-mobile/issue-4/Page8.js
+++ src/components/magazines-mobile/issue-4/Page8.js
@@ -1,32 +1,23 @@
-import React from 'react';
-import styles from './page8.module.scss';
+import React from 'react'
+import styles from './page8.module.scss'
 
 const Page8 = () => {
   return (
     <div className={styles.wrapper}>
-      <img src="/magazine/3/images/09_10/image1.png" className={styles.image3}/>   
-      <img src="/magazine/3/images/09_10/image2.png" className={styles.image4}/>   
-      <p className={styles.text2}>We’ve handed over our personal information so that some advertiser gets to shill us products. Worse, once we’ve handed it over, there’s no telling how any of these faceless entities use or store them. Data breaches are a constant occurrence. Countless terabytes of personal information have been exposed, and will continue to be exposed for as long as there is one single point of failure housing such data.
-        <br/><br/>
-The very same companies that once opened the door to information and personal expression have essentially become the very same monopolies they once sought to topple. And they have trillions of reasons why they should keep their dictatorship firmly entrenched as the gatekeepers and tastemakers for the people.
-<br/><br/>
-But the advent of web3 is just about to change that.
-<br/><br/>
-Web3 and the case for dynamism<br/>
-Web3’s capabilities are the logical answers to the most insidious aspects of web2, and its inherently open, decentralized nature is perfectly suited to reap the benefits of dynamism. Web3 brings back the agency of creators and consumers to truly own wealth and share in the economic gains of the ecosystems they choose to participate in. NFTs have provided value in allowing creators to take back ownership of their digital creations and build new ways to express oneself in the emerging metaverse. Digital identities will come to pass since your data belongs to you again—the blockchain has no need for KYC for them to keep tabs on your activities. 
-<br/><br/>
-Then, in the next emerging blockchain trend, the decentralized autonomous organizations (DAOs)  that redefine ownership, which tend to value social clout over eye-watering financial potential. Web3 natives holding governance tokens of the “tribe” they might belong to gives them the voice, clout, and agency to facilitate management and decision-making to improve the state of the community and its members—all of which have their skin in the game in the survival and success of the organization they belong to. That, as opposed to the Gone are the days of platforms overstepping the bounds of free speech and intellectual property ownership—this is the new Magna Carta for the new internet age.
-<br/><br/>
-Web3 has provided a dynamic answer to the worst excesses of web2 to provide a fair, more equitable share in ownership, incentives, reputation, and governance, in an open, immutable, and transparent environment. 
-<br/><br/>
-Open, dynamic systems across culture, economics, science, and art diminish the stranglehold of centralized authority in such fields—the proponents and self-proclaimed gatekeepers and tastemakers of which have decided that the nuanced problems that pervade our society should all conform to a single formula. 
-<br/><br/>
-And, in spite of the agents of stasis that have sought time and again to suppress the emergence of a better deal, a clear path for dynamic thinkers to build towards progress lies right in front of us: web3. 
-<br/><br/>
-It’s time to know the real enemy of all change: stasis.
-</p>       
+      <img src='/magazine/4/images/0708_background2-mobile.png' className={styles.image1}/>
+      <div className={styles.rect1}>
+      </div>
+      <div className={styles.text1}>
+        It is essentially the next logical evolution of Commerce, just as what has been happening in the paradigm shift from web2 to web3. DeCo is Commerce 3.0.
+        <br /><br />
+        How is it different? Much like web1 and web2, Commerce Mk. I and II were highly centralised, authoritarian structures unilaterally dictating how each aspect of their business is to run. While some provided a degree of autonomy for certain business units or branches to make decisions for themselves, all of those needed the blessing of a central scrutinizer.
+        <br /><br />
+        Nevertheless, e-commerce has become an inseparable part of our routine. Hundreds of billions of dollars in transactions change hands on an annual basis, and as more individuals gain access to the internet for the very first time, just as the proportion of commercial transactions happening online increases.
+        <br /><br />
+        The overwhelming majority of this online commerce is carried out through Commerce 2.0 marketplaces such as Amazon, eBay, and Alibaba, just to name a few. These platforms offer the service of allowing shoppers to have a universe of goods and services to purchase, all with the convenience of having to use just one website.         
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Page8;
+export default Page8
--- src/components/magazines-mobile/issue-4/Page9.js
+++ src/components/magazines-mobile/issue-4/Page9.js
@@ -1,51 +1,22 @@
 import React from 'react'
 import styles from './page9.module.scss'
-import Link from 'next/link';
 
 const Page9 = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.video}>
-        <video  muted loop autoPlay className={styles.video1}>
-          <source src="./magazine/3/images/11_12/video1.mp4" type="video/mp4" />
-        </video>
+      <img src='/magazine/4/images/1112_image1.png' className={styles.image1} />
+      <img src='/magazine/4/images/1112_image1.png' className={styles.image2} />
+      <div className={styles.rect1}></div>
+      <div className={styles.text1}>
+        It’s no pittance, either–while fees may vary depending on the platform, they can be as high as over 10% per item sold, regardless of the brand and the category. Buyers essentially cover this cost by buying at higher prices, while sellers’ costs are already priced in. The platforms make bank no matter what happens.
+
+        There’s no question that the model works. With a $1.7 trillion market capitalization as of this writing, Amazon has trillions of reasons why. They’re not running a charity, if that still isn’t abundantly clear to everyone by this time. Jeff Bezos needs to eat, dammit.
+
+        But therein lies the rub.
       </div>
-      
-      <div className={styles.image2}></div>
-      <div className={styles.image3}></div>
-      <div className={styles.image4}></div>
-      <div className={styles.image5}></div>
-      <div className={styles.image6}></div>
-      <p className={styles.text5}>1. You’ve had a front row seat and back stage pass to the entire run up of visual NFTs. Why do you think now is the right time for audio NFTs?</p>
-      <p className={styles.text6}>It's easier to land a placement for an audio file than a visual file. The first stage of NFT collecting was patronage - collectors supported artists to empower their craft. The next chapter is true ownership - and with that, there are more </p>
-      <p className={styles.text21}>proven channels for audio to be monetized (think Spotify, Audius, podcasts, etc.). We're going to see a huge wave of artists looking to share master royalties with their fans and audio NFTs will be the main way in which this is done.</p>
-      <p className={styles.text7}>2. Not everyone knows this but you actually studied the business of music in school, how has what you’ve experienced in web3 both in general and in music specifically either changed your perspective on what you were taught or contradicted what you were taught?</p>
-      <p className={styles.text8}>Nice homework! Having started my career in music and then going full-time crypto, I was able to take a step back and look at the music industry with a new lens. The entertainment at large is structured very differently from the start-up world, and with it comes a new sets of opportunities and challenges. Working in web3 has shown me that giving true ownership to your community is the only path forward. A lot of artists in music preach a two-way relationship with their fans, but very very few are actually doing it well. I believe that crypto is the way for artists to be incentivized and empowered by engaging with their fans on a more meaningful level.</p>
-      <img src="/magazine/3/images/11_12/image2.png" className={styles.image7}/>  
-      <img src="/magazine/3/images/11_12/image3.png" className={styles.image8}/>  
-      <img src="/magazine/3/images/11_12/image1.png" className={styles.image1}/>   
-      <img src="/magazine/3/images/11_12/image10.png" className={styles.image16}/>  
-      <p className={styles.text9}>3. We’ve seen a variety of projects experimenting with social tokens. WTF are social tokens and what do you think is special about them? </p>
-      <p className={styles.text10}>Social tokens are digital assets that represent a brand, individual or community. Social tokens give projects that historically not be well suited to raise outside capital an opportunity to source capital and deploy it as a community. It adds a dimension of shared ownership and upside that allows any contributor to earn financial and social capital from their work. Social tokens will power the Creator Economy and the rise of micro-economies.</p>
-      <p className={styles.text11}>4. Have you seen any interesting projects combining audio NFTs and social tokens?</p>
-      <p className={styles.text12}>The closest to it is Mirror. They offer tools for crowdfunding through both NFTs and social tokens. With Mirror, you can back a crowdfund and receive a social token in return. The act of backing the crowdfund could also come with an NFT attached to it. Right now, most creators are tying together 2-3 separate platforms (Catalog, Mirror, Foundation, NIFTEX) to experiment with fractionalization - the easiest way to add a social token component to any NFT. I'm most excited for a forthcoming platform called Fractional to be a leading player in fractionalization and I expect audio NFTs to be a big part of its ecosystem.</p>
-      <p className={styles.text13}>Coopahtroopa</p>
-      <p className={styles.text19}>Coopahtroopa</p>
-      <p className={styles.text20}>Coopahtroopa</p>
-      <p className={styles.text14}>5. So, where to next?</p>
-      <p className={styles.text15}>The Creator Economy is here to stay. I'm spending most of my time these days on DAOs as a direct onramp to wider Creator Economy sustainability. By giving a digital community a foundation through a DAO, we can start to see really cool experiments like NFTs at concerts, governance over festival lineups and much much more. I couldn't be more excited to watch the Creator Economy take crypto mainstream in the next 3-5 years.</p>
-      <img src="/magazine/3/images/11_12/image3.png" className={styles.image9}/>  
-      <p className={styles.text16}>READ MORE FROM COOPAH... YES?!</p>
-      <Link href="https://alias.co/coopahtroopa/essays">
-        <a target="_blank">
-          <p className={styles.text17}>DOWN THE RABBIT HOLE</p>
-        </a>
-      </Link>
-      <Link href="https://twitter.com/Cooopahtroopa">
-        <a target="_blank">
-          <p className={styles.text18}>Coopahtroopa</p>
-        </a>
-      </Link>
+      <div className={styles.text2}>
+        Because it’s not just about putting up an online marketplace to bring buyers and sellers together. You are, after all, the product. Suffice to say, these companies gather data about their users, their preferences, and their purchasing and selling habits. And, as with most commerce platforms, transactions are made using payment methods that require KYC. This allows them to build a dossier on every single one of its users–which is information that you otherwise wouldn’t necessarily want to be giving away freely. Much like web2, your data is entirely at the mercy of the platforms you use.
+      </div>
     </div>
   )
 }
