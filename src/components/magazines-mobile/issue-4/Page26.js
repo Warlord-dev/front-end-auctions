30ed68a374ed4f855e5d366641f55418532a4d54
James Lin <wonyy1988@outlook.com>
2021-08-12 08:44:25 -0700

    feat: add page 26~30 for mobile  Please enter the commit message for your changes. Lines starting

--- src/components/magazines-mobile/issue-4/Page26.js
+++ src/components/magazines-mobile/issue-4/Page26.js
@@ -4,6 +4,21 @@ import styles from './page26.module.scss'
 const Page26 = () => {
   return (
     <div className={styles.wrapper}>
+      <img className={styles.image1} src='/magazine/4/images/2728_background1_mobile.png' />
+      <img className={styles.image2} src='/magazine/4/images/2728_diamond_mobile.png' />
+      <div className={styles.text1}>
+        Open Source Oasis        
+      </div>
+      <div className={styles.text2}>
+        Open<br />
+        Web        
+      </div>
+      <div className={styles.text3}>
+        L i b r e S o f t w a r e        
+      </div>
+      <div className={styles.text4}>
+        L i b r e S o f t w a r e        
+      </div>
     </div>
   )
 }
--- src/components/magazines-mobile/issue-4/Page30.js
+++ src/components/magazines-mobile/issue-4/Page30.js
@@ -4,6 +4,14 @@ import styles from './page30.module.scss'
 const Page30 = () => {
   return (
     <div className={styles.wrapper}>
+      <img className={styles.image1} src='/magazine/4/images/3536_background2_mobile.png' />
+      <img className={styles.image2} src='/magazine/4/images/3536_image2.png' />
+      <img className={styles.image3} src='/magazine/4/images/3536_image2.png' />
+      <img className={styles.image4} src='/magazine/4/images/3536_image2.png' />
+      <img className={styles.image5} src='/magazine/4/images/3536_image2.png' />
+      <img className={styles.image6} src='/magazine/4/images/3536_image2.png' />
+      <img className={styles.image7} src='/magazine/4/images/3536_image2.png' />
+      <h1>OURZORA</h1>
     </div>
   )
 }
