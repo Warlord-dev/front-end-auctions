ea820a73e344e421c5a86f0a0a9facde01252cb6
James Lin <wonyy1988@outlook.com>
2021-08-18 20:00:37 -0700

    feat: add page 109, 110 for mobile magazine issue 4

--- src/components/magazines-mobile/issue-4/Page109.js
+++ src/components/magazines-mobile/issue-4/Page109.js
@@ -7,6 +7,33 @@ const Page109 = () => {
       <div className={styles.rect1}></div>
       <div className={styles.rect2}></div>
 
+      <div className={styles.textPatternWrapper}>
+        <div className={styles.textPattern}>
+          3D Bookshelf Project
+        </div>
+        <div className={styles.textPattern}>
+          3D Bookshelf Project
+        </div>
+        <div className={styles.textPattern}>
+          3D Bookshelf Project
+        </div>
+        <div className={styles.textPattern}>
+          3D Bookshelf Project
+        </div>
+        <div className={styles.textPattern}>
+          3D Bookshelf Project
+        </div>
+        <div className={styles.textPattern}>
+          3D Bookshelf Project
+        </div>
+        <div className={styles.textPattern}>
+          3D Bookshelf Project
+        </div>
+        <div className={styles.textPattern}>
+          3D Bookshelf Project
+        </div>
+      </div>
+
       <div className={styles.text1}>
         XENOTECH
       </div>
@@ -19,6 +46,29 @@ const Page109 = () => {
       <div className={styles.text4}>
         The main concept is a visual 3D bookshelf that you can use to track your book collecting and reading habits. The bookshelf would start blank and when you read a new book that you want to add to the shelf, you could scan your physical book and then a 3D model of that book would automatically be generated. Then you can display these 3D books on your shelf, share your shelf with your friends/social media, or pull up your 3D bookshelf in AR with your phone.
       </div>
+
+      <img src='/magazine/4/images/117118_image1.png' className={styles.image1} />
+
+      <img src='/magazine/4/images/117118_image1.png' className={styles.image2} />
+      <img src='/magazine/4/images/117118_image1.png' className={styles.image3} />
+      <img src='/magazine/4/images/117118_image1.png' className={styles.image4} />
+      <img src='/magazine/4/images/117118_image1.png' className={styles.image5} />
+      <img src='/magazine/4/images/117118_image1.png' className={styles.image6} />
+
+      <img src='/magazine/4/images/117118_image2.png' className={styles.image7} />
+
+      <video autoPlay muted loop className={styles.video1} >
+        <source src='./magazine/4/videos/117118_video1.mp4' type='video/mp4' />
+      </video>
+
+      <div className={styles.text5}>
+        So far I've built this prototype using my own scans I took of books, then using those scans I created these simple models of the books and bookshelf in Blender, before bringing everything together using the three.js Javascript library.        
+      </div>
+      <div className={styles.text6}>
+        So far I've built this prototype using my own scans I took of books, then using those scans I created these simple models of the books and bookshelf in Blender, before bringing everything together using the three.js Javascript library.        
+      </div>
+
+      
     </div>
   )
 }
