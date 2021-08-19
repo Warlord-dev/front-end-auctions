2050e1fa04fc3c5f5758c266767cfadbb8cb7a91
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2021-08-19 02:31:58 -0700

    update text

--- src/components/magazines/issue-4/Page34.js
+++ src/components/magazines/issue-4/Page34.js
@@ -3,51 +3,46 @@ import styles from './page34.module.scss'
 const Page34 = () => {
   return (
     <div className={styles.wrapper}>
-      <p className={styles.text1}>The Time Dealer.</p>
+      <p className={styles.text1}>The Art in Post Selection.</p>
       <p className={styles.text2}>
-        Time is the most abundant asset in our universe. The most abundant, yet probably the most
-        underutilised. That might sound like a somewhat intentionally mystifying statement, but it’s
-        not. Because far too many of us are dictated by it. We accept the points of freedom and
-        points of constraint that it imposes over our lives, and we mould so much of our existence
-        around it, whether we like it or not; from it determing what we call “breakfast”, to the 9 -
-        5, to the broader meta timeline of go to school, get a job, have a weekend, achieve a
-        work-life balance, repeat...completely chained to the reason.
+        Right now I’m making a giant remix of everything that has come before and everything that is 
+        to come after. Right now I’m playing with these time crystals. I’m in this liminal place 
+        between where I’ve been and where I’m going, and I’m putting everything on the line. It might 
+        feel a little uncomfortable sometimes, even a little scary. But the truth is, this doesn’t 
+        really bother me too much. In fact, I’m kind of addicted to it. I’m sticking with it. This is 
+        an all in game. A half-hearted spirit has no power. Because I know exactly what direction I’m 
+        headed and who’s chosen to come along for the ride. It’s a ride filled with intense passion. 
+        And something good is happening.
         <br />
         <br />
-        So, how do we break this frame? How do we unfreeze this underutilised asset and inject
-        volume and liquidity into every aspect of it? How do we literally make the market so that we
-        can start dealing it? So that we can actually start living with real agency.
+        As we cultivate this new phase of matter it’s easy to get pulled into a gravity well of 
+        collapsing paridigms and tempt drifting past the event horizon. Because being content with how 
+        things are is how we slip into an area of low mass, there’s nothing to bump into or push off from. 
+        It’s a grey zone of rebalancing. There’s no clash to stir up catalysts of new innovation and new 
+        hope. Again and again for many this slow fade into inertia just keeps repeating. 
         <br />
         <br />
-        Because, after all, if we can’t learn to deal time, learn to manipulate it to our advantage,
-        to create and destroy it, then we will always be running against the clock— exerting energy
-        and going against favourable inertia. I, myself, have always been incredibly time conscious.
-        It was never a feeling of anxiety, but, rather a consistently irrepressible urgency to stay
-        ahead. To make sure that at each moment I am not just unintentionally taking this asset for
-        granted, or yielding to someone else’s preconceived frame of how I should act, operate, or
-        be in any moment.
+        So where does a real spark come from? Where is the magic beans moment that against all odds 
+        carries us up and out. It certainly doesn’t come from brute force or persistant logic. Instead, 
+        the champion moment can be instigated through our selective ability to hit the magic reset button.
+        Because the past is never where you think that you left it. This is a tool that allows us to bypass 
+        irrational fantasies and skip right to the good stuff. The post select. We can shape our desires and
+        realities even as it they shape us. This isn’t simply a matter of delicate interpretation. It’s how 
+        the complete spread from our neurons to the encompassing universe work. Backwards causation gives a
+        broader perspective. We zoom out in order to zoom in. 
         <br />
         <br />
-        When I have the means to take a leap, take action, and have huge impact in my life that can
-        help so many people— why accept anything less? Why wait to take action? And why allow time
-        to play any role in oppressing the outcome.
+        Fortunately, decentralised tech in commerce gives us these kind of super post selecting powers, where 
+        we can build around a core itterable loop of economic activity that creates wealth in the core of 
+        the entire mechanism, gaining time, space and greater tranformational ability as the friction points 
+        of the system are radically reduced and our ability to communicate, transact, trade, buy, sell, wear 
+        generously compounds on the fundamental value collectively generated. 
         <br />
         <br />
-        It starts by smashing through the binary. It’s not about accepting it as is or even going
-        against it. It’s about rewinding the conditions so that we can start early. We can find the
-        pressure points of the sytem and apply the slightest adjustments to then change any thing,
-        person or concept that is being foisted upon us. This way, time becomes just another highly
-        desirable tool with deep cross context interoperability.
-        <br />
-        <br />
-        This is why DeFi and web3 are just so revolutionary. Because these protocols, gadgets,
-        DAPPs, custom built incentives and the open infrastructure that it’s all formulated upon
-        allows us to establish our own rules. Whether that be in our path to achieving financial
-        freedom or just removing consistent points of friction in commerce, business, our lives and
-        more. Either way, we can establish our own rules. We no longer play someone else’s game.
-        <br />
-        <br />
-        We can deal time, on our terms.{' '}
+        Decentralised Commerce is not just about moving goods from one point to another. It’s much more than 
+        that. It fosters and encourages fundamental breakthroughs of economic, creative and financial power 
+        so that we can proactively shape our present, no longer constrained but rather amplified by the past. 
+        For there are many kinds of magic, after all. 
       </p>
       <p className={styles.text12}>- Emma-Jane MacKinnon-Lee</p>
       <img src="./magazine/4/images/0304_background.jpg" className={styles.image1} />
