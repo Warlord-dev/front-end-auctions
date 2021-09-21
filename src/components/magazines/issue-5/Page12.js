38ca2007161f9c753d4de9b5421b0aa0a57373b0
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2021-09-21 10:31:44 -0700

    update

--- src/components/magazines/issue-5/Page12.js
+++ src/components/magazines/issue-5/Page12.js
@@ -4,7 +4,7 @@ const Page12 = () => {
   return (
     <div className={styles.wrapper}>
       <h1>DID</h1>
-      <p className={styles.text1}>Comma  | The inalienable right to self sovereign ownership. It’s the recognition that life, liberty and the pursuit of happiness are rooted in the practicle exercise of property and free exchange. DID is the skeleton key to your personal realms.</p>
+      <p className={styles.text1}>Comma  | The inalienable right to self sovereign ownership. It’s the recognition that life, liberty and the pursuit of happiness are rooted in the practical exercise of property and free exchange. DID is the skeleton key to your personal realms.</p>
     </div>
   );
 };
