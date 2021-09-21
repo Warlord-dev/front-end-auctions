aaf2b77f33d051c5dea95da5a758a64667f3543f
James Lin <wonyy1988@outlook.com>
2021-09-21 11:00:38 -0700

    fix: page2 on issue5

--- src/components/magazines/issue-5/Page12.js
+++ src/components/magazines/issue-5/Page12.js
@@ -5,6 +5,9 @@ const Page12 = () => {
     <div className={styles.wrapper}>
       <h1>DID</h1>
       <p className={styles.text1}>Comma  | The inalienable right to self sovereign ownership. It’s the recognition that life, liberty and the pursuit of happiness are rooted in the practicle exercise of property and free exchange. DID is the skeleton key to your personal realms.</p>
+      <div className={styles.text3}>
+        Decentralised Identity
+      </div>
     </div>
   );
 };
