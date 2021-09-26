f543e7100ffe305157006e0c9f29191f3dbde1bf
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2021-09-26 13:54:18 -0700

    update index

--- src/pages/all-issues/index.js
+++ src/pages/all-issues/index.js
@@ -16,7 +16,7 @@ const AllIssues = () => {
                 image={issue.image}
                 id={issue.id}
                 title={issue.title}
-                link1={`/magazines/${index + 1}`}
+                link1={`/magazines/${5 - index}`}
                 link2={`/unlockable/${index + 1}`}
               />
             </div>
