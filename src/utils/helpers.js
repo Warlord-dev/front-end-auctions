886dc50fa7ee139d40e23fdd62a553a6d5ecf20b
jupiteruwdev <harutoinoue8@gmail.com>
2021-10-10 21:25:55 -0400

    helpers minor issues fixed.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8,7 +8,7 @@ export const reviseUrl = (url) => {
 export const getRarityId = (rarity) => {
   if (rarity === 'Exclusive') {
     return 1;
-  } else if (rarity === 'Semi-Rare') {
+  } else if (rarity === 'Semi-Rare' || rarity === 'Semi Rare') {
     return 2;
   } else {
     return 3;
@@ -23,19 +23,22 @@ export const getRarity = (rarity) => {
   } else {
     return 'Common';
   }
-}
+};
 
 export const filterProducts = (prods, filter, sortBy) => {
   let filteredProducts = [...prods];
   if (filter.length) {
     filteredProducts = prods.filter(
-      (prod) => prod.designer?.name.toLowerCase().includes(filter.toLowerCase()) 
-      || prod.garment?.name.toLowerCase().includes(filter.toLowerCase())
-      || prod.owners?.find(item => item.username && item.username.toLowerCase().includes(filter.toLowerCase()))
-    )
+      (prod) =>
+        prod.designer?.name.toLowerCase().includes(filter.toLowerCase()) ||
+        prod.garment?.name.toLowerCase().includes(filter.toLowerCase()) ||
+        prod.owners?.find(
+          (item) => item.username && item.username.toLowerCase().includes(filter.toLowerCase()),
+        ),
+    );
   }
   if (sortBy) {
-    switch(sortBy) {
+    switch (sortBy) {
       case '1':
         filteredProducts.sort((a, b) => {
           if (parseInt(a.startTime) < parseInt(b.startTime)) return 1;
@@ -62,7 +65,9 @@ export const filterProducts = (prods, filter, sortBy) => {
         });
         break;
       case '4':
-        filteredProducts = filteredProducts.filter((prod) => prod.auction && parseInt(prod.endTime) < Date.now() / 1000);
+        filteredProducts = filteredProducts.filter(
+          (prod) => prod.auction && parseInt(prod.endTime) < Date.now() / 1000,
+        );
         break;
       case '5':
         filteredProducts = filteredProducts.filter((prod) => prod.auction);
@@ -82,22 +87,10 @@ export const filterProducts = (prods, filter, sortBy) => {
     }
   }
   return filteredProducts;
-}
+};
 
-export const generateLookImage = item => {
-  const {
-    id,
-    name,
-    background,
-    color,
-    texture,
-    pattern,
-    shape,
-    flare,
-    form,
-    line,
-    element
-  } = item
+export const generateLookImage = (item) => {
+  const { id, name, background, color, texture, pattern, shape, flare, form, line, element } = item;
 
   const imageString = `
     <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>@import url("https://fonts.googleapis.com/css?family=Inknut+Antiqua:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic"); .base { fill: white; font-family: Inknut Antiqua; font-size: 10.8px; font-weight:bold; }</style><rect width="100%" height="100%"
@@ -109,7 +102,7 @@ export const generateLookImage = item => {
       <text x="10" y="140" class="base">${flare}</text>
       <text x="10" y="160" class="base">${shape}</text>
       <text x="10" y="180" class="base">${form}</text>
-      <text x="10" y="200" class="base">${element}</text></svg>`
-  
-  return 'data:image/svg+xml;base64,' + btoa(unescape(encodeURIComponent(imageString)))
-}
+      <text x="10" y="200" class="base">${element}</text></svg>`;
+
+  return 'data:image/svg+xml;base64,' + btoa(unescape(encodeURIComponent(imageString)));
+};
