d0ab4caa356ca55bf5149508e12923ae60edf610
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2021-10-21 04:17:37 -0700

    fix

--- src/components/magazines/issue-6/Page5556.js
+++ src/components/magazines/issue-6/Page5556.js
@@ -16,17 +16,17 @@ const Page5556 = () => {
       <div className={styles.text1}>
         I've been creating 3D models since 2015. I enrolled in a university here in the Philippines
         as a Bachelor of Science in Information Technology majoring in Animation and Game
-        Development. I always envision myself to be part of something big and I thought making games
+        Development. I always envisioned I would be part of something big and I thought making games
         was really a thing that I might enjoy since I was dreaming to make one ever since I was a
-        kid but I just didn’t know how back then. Creating art has always been my passion and seeing
-        it inside a game where everyone enjoys gives me a different kind of excitement. I myself
+        kid, but I just didn’t know how back then. Creating art has always been my passion and seeing
+        it inside a game where everyone enjoys it, gives me a different kind of excitement. I myself
         don’t like my art but seeing others appreciate what I do just fills me up with joy.
       </div>
       <div className={styles.text2}>
         <b>Favourite 3D software?</b>
       </div>
       <div className={styles.text3}>
-        I love Blender because it's open source and you can do beyond so many things! My favorite
+        I love Blender because it's open source and you can go beyond! My favorite
         software for Sculpting 3D is Zbrush since it offers a variety of tools and I think it’s
         simply the best software for creating organic creatures. For hard surface modelling or any
         game props, I use Maya since it’s what I was taught in college and I was very used to the
@@ -42,27 +42,27 @@ const Page5556 = () => {
         every update.
       </div>
       <div className={styles.text4}>
-        At first, NFT was a vague concept to me but I was already amazed by crypto currency in
+        At first, NFTs was a vague concept to me but I was already amazed by crypto currency in
         general so I ended up investing some of my savings into crypto after doing some research.
       </div>
       <div className={styles.text5}>
         Looking for a job opportunity here after graduating from college is quite challenging
         because every company is looking for someone who has experience already in a certain field.
         Having said that, it was pre-pandemic when I was hired at a startup gaming company which I
-        absolutely loved and I really enjoy doing. For all I thought my dream was finally moving
-        forward then, the Covid 19 came along and the company didn’t have enough power to sustain us
+        absolutely loved and I really enjoyed. For all I thought my dream was finally moving
+        forward then Covid 19 came along and the company didn’t have enough power to sustain us
         which led us to be laid off. Looking for another job after that was a tough journey, so I
         decided to try 3D printing but that didn’t go so well either.
         <br />
         <br />I was thinking of other ways to earn money online, and was looking for investment
-        opportunities until I saw Sandbox. Fortunately, they had an opening post as a freelance
+        opportunities when I saw Sandbox. Fortunately, they had an opening post as a freelance
         Creator Fund Artist so I tried my luck applying. To my surprise, I passed the exam and have
         been doing voxel art since then. The atmosphere that they brought made me motivated until I
         got an offer for a full-time job. The team is simply amazing and talented but what made me
         stick to Sandbox is how they care about their artists.
       </div>
       <div className={styles.text6}>
-        On Sandbox we do a variety of assets with certain themes which are often exciting such as
+        In Sandbox we do a variety of assets with certain themes which are often exciting; such as
         mechas, halloween, and Christmas. You can take a look at them{' '}
         <a
           href="https://www.sandbox.game/en/users/kradsupersoldier/95619c69-f6af-4335-9d1c-d23cab9275c3/"
@@ -75,45 +75,45 @@ const Page5556 = () => {
       <div className={styles.text7}>
         Why Sandbox? Just like what I’ve mentioned, it makes me want to stay and be loyal to them
         because of how they treat us artists. They never forget to give us the right compensation
-        and the opportunities that they opened for us that would never be available if it weren’t
+        and the opportunities that they have opened for us would never be available if it weren’t
         for them. Our first NFT sale was a major success all thanks to their support.
         <br />
         <br />I believe that the pandemic made us realize there is also life in the metaverse. The
         concept of P2E brought food to the table for people like me who were laid off because of the
         Covid 19 virus. These games made us enjoy life and support our families at the same time
-        although it’s not yet stable since it’s still a new concept to us, I believe in the future
-        that it holds for everyone. Decentralization made us realize how free we can be with our
-        finances that benefits everyone. I think we are still early in these stages but it has been
-        looking very good from this point. I think the metaverse will soon be our new reality as
-        time goes by and it can be both good and bad. It’s bad if we forget how to live in the real
-        world but it’s great if we can benefit from it so we can live our life in the real world.
+        although. I believe in the future that it holds for everyone. Decentralization made us 
+        realize how free we can be with our finances, and that truly benefits everyone.
+        I think we are still early in these stages but it has been looking very good from this point.
+        I think the metaverse will soon be our new reality as time goes by and it can be both good 
+        and bad. It’s bad if it leads us away from real connection, but at the same time, all signs
+        are showing that it's only going to catalyse our relationships further. 
       </div>
       <div className={styles.text8}>
         The biggest challenge for me as an artist and I think I speak for a lot of artists out
         there, is that we don’t get valued as much in the real world. A lot of creative and artistic
         minds don’t get paid as much as they deserve because they feel so low about themselves that
-        they are willing to accept any value people think they’re worth but with NFT’s, it makes
-        people like us special. It shows us our worth as an artist and it brought everyone
-        confidence in their art. Even memes are welcome and I think the community has been really
-        generous about NFTs and I hope it could continue on and fix every artist’s sense of self
+        they are willing to accept any value people think they’re worth. But with NFT’s, it makes
+        people like us special. It shows us our worth as artists and it has brought everyone
+        confidence in their art. Even memes are welcome! I think the community has been really
+        generous about NFTs and I hope it can continue on and fix every artist’s sense of self
         worth. For Web3 I think it mostly values you as a person where you aren’t seen as a product
-        anymore and which gives you more privacy and control over your own assets.
+        anymore, giving you more privacy and control over your own assets.
         <br />
         <br />
         To all the artists out there, I know how it can be scary to try a new path for our creative
-        journey and sometimes we only want to try the things that other people already did however I
-        think the universe has a way of creating new paths for us that may result to either good or
-        bad but if we keep going through the same path over and over again, how do we experience
+        journey and sometimes we only want to try the things that other people already have paved, 
+        however I think the universe has a way of creating new paths for us that may result to either 
+        good or bad, but if we keep going through the same path over and over again, how do we experience
         change and growth? If people are not seeing value in your works it means that your place
-        belongs somewhere else where you could fit in and who knows, it could be the metaverse.
-        Alongside this, I would always recommend doing your own research before jumping on anything.
+        belongs somewhere else, it could be the metaverse. Alongside this, I would always recommend doing
+        your own research before jumping on anything.
         <br />
         <br />
-        Lots of creative stuff coming for the next few months. Of course, the most exciting part is
-        still being part of The Sandbox, we saw how other games grew so big and The Sandbox already
-        built a community that opened up doors for other communities as well. I’ve also been working
-        on 2 NFT collections at the side, which is VoxaMech and PixaSouls. VoxaMech is a voxel art
-        animated mecha card that I'm planning to release on Opensea but there will also be an
+        Lots of creative stuff is coming for the next few months. Of course, the most exciting part is
+        still being part of The Sandbox, we saw how other games grew and The Sandbox already has
+        built a community that has opened up doors for other communities as well. I’ve also been working
+        on 2 NFT collections on the side, which is VoxaMech and PixaSouls. VoxaMech is a voxel art
+        animated mecha card that I'm planning to release on Opensea, but there will also be an
         animated game asset available soon in the TSB marketplace so watch out for that. Pixelsouls
         is a passion project that’s also an NFT collection that has 7777 unique souls that will soon
         be available on Opensea as well. If we’re lucky to have a successful launch, we also plan to
