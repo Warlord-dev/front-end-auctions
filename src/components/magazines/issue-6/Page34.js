793ca2817e321226c845b3ea93d8303f45a572d7
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2021-10-21 13:58:46 -0700

    fix

--- src/components/magazines/issue-6/Page34.js
+++ src/components/magazines/issue-6/Page34.js
@@ -4,12 +4,20 @@ const Page34 = () => {
   return (
     <div className={styles.wrapper}>
       <img src='./magazine/6/images/0304_image1.png' className={styles.image4} />
-      <p className={styles.text1}></p>
+      <p className={styles.text1}></p> Olympic Sneakers of Conviction.</p>
       <p className={styles.text2}>
-        
-      </p>
-      <p className={styles.text12}>- Emma-Jane MacKinnon-Lee</p>
-      <img src='./magazine/6/images/0304_image2.jpg' className={styles.image1} />
+      As we swim through ever-deepening layers of complexity and exhiliration in building out the metaverse and its multidimensional realms, it’s far too predictable that some of that us will get caught up in the short sighted hope of being able to guess the winning lotto numbers of the market each day.
+      <br /><br />
+      And in a live market like web3, the distance between hype and reality is so instantly reactive and kinetic that anyone not grounded and anchored in the convictions that they have, for the reality they are weaving and narrative they are continually editing, puts themselves in a position of perpetuating vulnerability to the point of becoming helpless. 
+      <br /><br />
+      The ideal for any performer is firmly grounded flexibility. Recognising that being at the pinacle of market leaderboards in the eyes of others is not usually correlated with the underlying strength of verifiable value when we scratch beneath the surface. And, it’s also no good in just waiting for the optimal conditions that snap the market back from fundamental detachment. Keeping an attuned eye for consistent psychological and technical themes of error only leads to cognitive dissonance. We get caught in the rhythm. 
+      <br /><br />
+      The best allegory is an old one lost to the ever decreasing performance quality of Google’s search algorithms about entrepreneurship and gold. It goes a little something like this; every man founder or leader of a new venture seeks out gold at the top of mountains. They sit around in their local adventurer’s watering hole hearing stories of those who’ve made it, who’ve climbed to unbelieveable heights and overcome treacherous ordeals. The vast majority of them failing at some point along the way. At some point, someone comes in and tells the assembled hungry entrepreneurs they’ve done it; they’ve found gold at the top of a local mountain. Out rushes everyone to climb that mountain and find their gold too. There is just one problem, that gold on that mountain has already been claimed by the party that came back to tell the tales of their heroics. 
+      <br /><br />
+      The road to gold at the top of a mountain requires tremendous tenacity to discover, reach, claim and return to tell the tale. The gold is real. But, it is rare and exists dispersed on out of the way mountains not yet discovered in terrain filled with the fog of information assymetry. The lesson is that in order to succeed you must have the courage of your conviction. Then find your own mountain. 
+     </p>
+     <p className={styles.text12}>- Emma-Jane MacKinnon-Lee</p>
+     <img src='./magazine/6/images/0304_image2.jpg' className={styles.image1} />
       
       <p className={styles.text3}>contributors</p>
       <p className={styles.text4}>
