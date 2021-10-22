51965874ec03a802f745e59e8b3b9141d18b4133
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2021-10-21 18:01:19 -0700

    Update Page2122.js

--- src/components/magazines/issue-6/Page2122.js
+++ src/components/magazines/issue-6/Page2122.js
@@ -58,7 +58,7 @@ const Page2122 = () => {
       </a>
 
       <audio ref={refAudio} loop>
-        <source src='/magazine/6/sounds/2122_sound1.mp3' type='audio/mpeg' />
+        <source src='/magazine/6/sounds/david2.mp3' type='audio/mpeg' />
       </audio>
       <div className={styles.text5}>
         Ana Rivera
