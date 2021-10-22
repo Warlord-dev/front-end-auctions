8ad8efd478bdce7097e84abe720701d419758242
wj-code-mania <wonyy1988@outlook.com>
2021-10-22 08:07:22 -0700

    build: fix

--- src/components/product-page-loader/index.js
+++ src/components/product-page-loader/index.js
@@ -17,14 +17,14 @@ const ProductPageLoader = (props) => {
         {...props}
       >
     <rect x="35" y="56" rx="12" ry="12" width="147" height="147" /> 
-    <rect x="52" y="37" rx="0" ry="0" width="117" height="14" /> 
-    <rect x="194" y="59" rx="0" ry="0" width="64" height="12" /> 
-    <rect x="194" y="78" rx="0" ry="0" width="133" height="12" /> 
-    <rect x="193" y="97" rx="6" ry="6" width="227" height="93" /> 
-    <rect x="269" y="201" rx="0" ry="0" width="76" height="22" /> 
-    <rect x="204" y="234" rx="0" ry="0" width="201" height="20" /> 
-    <rect x="204" y="260" rx="0" ry="0" width="201" height="20" /> 
-    <rect x="204" y="285" rx="0" ry="0" width="201" height="20" /> 
+    <rect x="49" y="36" rx="0" ry="0" width="117" height="14" /> 
+    <rect x="199" y="58" rx="0" ry="0" width="64" height="12" /> 
+    <rect x="199" y="77" rx="0" ry="0" width="133" height="12" /> 
+    <rect x="198" y="96" rx="6" ry="6" width="227" height="93" /> 
+    <rect x="275" y="201" rx="0" ry="0" width="76" height="22" /> 
+    <rect x="210" y="234" rx="0" ry="0" width="201" height="20" /> 
+    <rect x="210" y="260" rx="0" ry="0" width="201" height="20" /> 
+    <rect x="210" y="285" rx="0" ry="0" width="201" height="20" /> 
     <rect x="0" y="362" rx="0" ry="0" width="480" height="179" />
       </ContentLoader>
     </div>
