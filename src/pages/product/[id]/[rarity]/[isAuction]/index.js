80d09ba5c9a72fc4506c7f55be7f416123eddc2b
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2021-10-22 20:12:23 -0700

    Update index.js

--- src/pages/product/[id]/[rarity]/[isAuction]/index.js
+++ src/pages/product/[id]/[rarity]/[isAuction]/index.js
@@ -8,7 +8,7 @@ import ImageCard from '@components/image-card';
 import InfoCard from '@components/info-card';
 import Container from '@components/container';
 import UserList from '@components/user-list';
-// import FashionList from '@components/fashion-list';
+import FashionList from '@components/fashion-list';
 import BannerBar from '@components/banner-bar';
 import PriceCard from '@components/price-card';
 import ProductPageLoader from '@components/product-page-loader';
@@ -97,25 +97,25 @@ const Product = ({ pageTitle }) => {
   const [isFetchedProduct, setIsFetchedProduct] = useState(false)
   const [isFetchedViewCount, setIsFetchedViewCount] = useState(false)
   const [isFetchedSecondDesigners, setIsFetchedSecondDesigners] = useState(false)
-//   const fashionData = [
-//     {
-//       title: 'DeFi Staking Functionality',
-//       description: `All NFTs can be staked in the DIGITALAX NFT Staking Contracts on Polygon for $MONA yield. This forms part of the broader Fashion x DeFi merger that DIGITALAX has undertaken to bring greater utility to metaversal fashion and also welcome multitudes more into web3 and DeFi. 
+  const fashionData = [
+    {
+      title: 'DeFi Staking Functionality',
+      description: `All NFTs can be staked in the DIGITALAX NFT Staking Contracts on Polygon for $MONA yield. This forms part of the broader Fashion x DeFi merger that DIGITALAX has undertaken to bring greater utility to metaversal fashion and also welcome multitudes more into web3 and DeFi. 
 
-//       What if you could earn more from what you wear? Wear to DeFi lets you put your fashion to work for you. We are melting the centralised exploitative crown to weave the fabric of a generative ecosystem.            
-//       `,
-//     },
-//     {
-//       title: 'Fractional Garment ERC-1155 Open Source Pattern',
-//       description: `Fractional Garment Ownership (FGO) sets forth the standard and dress code for the manufacture of digital fashion along the content supply chain. FGO leverages ERC Protocol standards across the Ethereum Blockchain and Polygon (Matic Network) for breaking down a master ERC-721 digital garment into its programmable and composable ERC-1155 elements of materials, patterns and textures.
+      What if you could earn more from what you wear? Wear to DeFi lets you put your fashion to work for you. We are melting the centralised exploitative crown to weave the fabric of a generative ecosystem.            
+      `,
+    },
+    {
+      title: 'Fractional Garment ERC-1155 Open Source Pattern',
+      description: `Fractional Garment Ownership (FGO) sets forth the standard and dress code for the manufacture of digital fashion along the content supply chain. FGO leverages ERC Protocol standards across the Ethereum Blockchain and Polygon (Matic Network) for breaking down a master ERC-721 digital garment into its programmable and composable ERC-1155 elements of materials, patterns and textures.
 
-//       Here, we are using a variant on the ERC-998 standard, where each ERC-721 token can hold a balance of ERC-1155 NFTs. We coin this respectively the Parent and Child NFTs. This allows for other designers to leverage off of the open source digital libraries, incorporating the patterns, materials and textures into their master garments.`,
-//     },
-//     {
-//       title: '3D Model File Included',
-//       description: `All of the DIGITALAX digital fashion garment and accessory ERC-721 NFTs are backed by the underlying 3D model FBX file, stored in IPFS. This forms part of the platform’s broader pursuit for decentralising content distribution and access to it. The FBX file is one of the most popular and widely used 3D data interchange formats between 3D editors and game engines. There are still efficiency problems that exist with it, which DIGITALAX is working to solve through it’s DASH File Format architecture. `,
-//     },
-//   ];
+      Here, we are using a variant on the ERC-998 standard, where each ERC-721 token can hold a balance of ERC-1155 NFTs. We coin this respectively the Parent and Child NFTs. This allows for other designers to leverage off of the open source digital libraries, incorporating the patterns, materials and textures into their master garments.`,
+    },
+    {
+      title: '3D Model File Included',
+      description: `All of the DIGITALAX digital fashion garment and accessory ERC-721 NFTs are backed by the underlying 3D model FBX file, stored in IPFS. This forms part of the platform’s broader pursuit for decentralising content distribution and access to it. The FBX file is one of the most popular and widely used 3D data interchange formats between 3D editors and game engines. There are still efficiency problems that exist with it, which DIGITALAX is working to solve through it’s DASH File Format architecture. `,
+    },
+  ];
 
   const sourceTypeDescription = {
     'AR FILTER': 'You can wear and view this fashion in AR',
@@ -743,7 +743,7 @@ const Product = ({ pageTitle }) => {
          <section id="fashion_list">
            {!id.includes('v1') &&
           ((parseInt(id) >= 11 && parseInt(id) < 100294) || parseInt(id) > 130000) ? (
-//             <FashionList fashionData={fashionData} collections={product} />
+             <FashionList fashionData={fashionData} collections={product} />
            ) : null}
          </section>
       </div>
